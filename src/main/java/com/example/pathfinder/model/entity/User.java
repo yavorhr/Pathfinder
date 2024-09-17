5e9a5420c09914fc1774c6d4c45aa15464dfd26c
wolfie <yavorhristozov@gmail.com>
2024-09-17 13:11:16 +0300

    Add : User entity
    - Implemented User entity class with the following fields :
    
       * Integer age
       * String fullName (nullable = false)
       * LevelEnum level (EnumType.STRING)
       * String password (nullable = false)
       * String username (unique = true,nullable = false)
    
    - Annotated entity with @Entity and @Table for the 'users' table
    - Added @ManyToMany relationship with Roles entity
    - Inherits BaseEntity @MappedSuperclass for the Long id field
    - Added getter and setter methods

---
+++ src/main/java/com/example/pathfinder/model/entity/User.java
@@ -0,0 +1,74 @@
+package com.example.pathfinder.model.entity;
+
+import com.example.pathfinder.model.entity.enums.LevelEnum;
+import jakarta.persistence.*;
+
+import java.util.Set;
+
+@Entity
+@Table(name = "users")
+public class User extends BaseEntity {
+  private Integer age;
+  private String fullName;
+  private LevelEnum level;
+  private String password;
+  private String username;
+  private Set<UserRoleEntity> roles;
+
+  public User() {
+  }
+
+  @Column
+  public Integer getAge() {
+    return age;
+  }
+
+  @Column(name = "full_name", nullable = false)
+  public String getFullName() {
+    return fullName;
+  }
+
+  @Enumerated(EnumType.STRING)
+  public LevelEnum getLevel() {
+    return level;
+  }
+
+  @Column(nullable = false)
+  public String getPassword() {
+    return password;
+  }
+
+  @Column(unique = true,nullable = false)
+  public String getUsername() {
+    return username;
+  }
+
+  @ManyToMany(mappedBy = "users")
+  public Set<UserRoleEntity> getRoles() {
+    return roles;
+  }
+
+  public void setAge(Integer age) {
+    this.age = age;
+  }
+
+  public void setFullName(String fullName) {
+    this.fullName = fullName;
+  }
+
+  public void setLevel(LevelEnum level) {
+    this.level = level;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  public void setRoles(Set<UserRoleEntity> roles) {
+    this.roles = roles;
+  }
+}
