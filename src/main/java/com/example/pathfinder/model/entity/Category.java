4b61f74d2c864ac494a2828a682890830c67ae16
wolfie <yavorhristozov@gmail.com>
2024-09-17 14:16:05 +0300

    Update Category.java
    Add mappedBy = "categories" to the @ManyToMany relation with routes

--- src/main/java/com/example/pathfinder/model/entity/Category.java
+++ src/main/java/com/example/pathfinder/model/entity/Category.java
@@ -3,11 +3,14 @@ package com.example.pathfinder.model.entity;
 import com.example.pathfinder.model.entity.enums.CategoryEnum;
 import jakarta.persistence.*;
 
+import java.util.Set;
+
 @Entity
 @Table(name = "categories")
 public class Category extends BaseEntity {
   private String description;
   private CategoryEnum name;
+  private Set<Route> routes;
 
   public Category() {
   }
@@ -15,6 +18,11 @@ public class Category extends BaseEntity {
   @Column(columnDefinition = "TEXT")
   public String getDescription() {
     return description;
+  }
+
+  @ManyToMany(mappedBy = "categories")
+  public Set<Route> getRoutes() {
+    return routes;
   }
 
   @Enumerated(EnumType.STRING)
@@ -29,5 +37,9 @@ public class Category extends BaseEntity {
 
   public void setName(CategoryEnum name) {
     this.name = name;
+  }
+
+  public void setRoutes(Set<Route> routes) {
+    this.routes = routes;
   }
 }
