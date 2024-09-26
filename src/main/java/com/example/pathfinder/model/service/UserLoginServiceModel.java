e7030ba7311f5a328101caf571c51887f3c56b22
wolfie <yavorhristozov@gmail.com>
2024-09-26 16:23:35 +0300

    Create UserLoginServiceModel.java

---
+++ src/main/java/com/example/pathfinder/model/service/UserLoginServiceModel.java
@@ -0,0 +1,23 @@
+package com.example.pathfinder.model.service;
+
+public class UserLoginServiceModel {
+  private Long id;
+  private String username;
+
+  public UserLoginServiceModel() {
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public UserLoginServiceModel setId(Long id) {
+    this.id = id;
+    return this;
+  }
+
+  public UserLoginServiceModel setUsername(String username) {
+    this.username = username;
+    return this;
+  }
+}
