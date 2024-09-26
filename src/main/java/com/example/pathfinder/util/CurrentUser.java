542dacb62c1fcdc97e887cc3f6005454abfecc64
wolfie <yavorhristozov@gmail.com>
2024-09-26 13:24:41 +0300

    Add : CurrentUser
    - Bean class, annotated with @Component and @SessionScope
    - It will held information about the logged in user until Spring Security is implemented

---
+++ src/main/java/com/example/pathfinder/util/CurrentUser.java
@@ -0,0 +1,23 @@
+package com.example.pathfinder.util;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.annotation.SessionScope;
+
+@Component
+@SessionScope
+public class CurrentUser {
+  private Long id;
+  private String username;
+
+  public CurrentUser() {
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public CurrentUser setId(Long id) {
+    this.id = id;
+    return this;
+  }
+}
