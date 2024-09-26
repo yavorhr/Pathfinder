b47c651ce364f5819ac4db447d8f956f918fedd6
wolfie <yavorhristozov@gmail.com>
2024-09-26 16:24:11 +0300

    Update UserLoginAndLogoutController.java
    Update the route to "/users/login"

--- src/main/java/com/example/pathfinder/web/UserLoginAndLogoutController.java
+++ src/main/java/com/example/pathfinder/web/UserLoginAndLogoutController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class UserLoginAndLogoutController {
 
-  @GetMapping("/login")
+  @GetMapping("/users/login")
   public String loginPage() {
     return "login";
   }
