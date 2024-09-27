b738e696bc8e34396d4dca5f811eeb81b354024c
wolfie <yavorhristozov@gmail.com>
2024-09-27 14:03:34 +0300

    Update UserProfileController.java
    -Add logic for returning UserProfileServiceModel from the UserService with the user data
    -Map the UserProfileServiceModel  to view model and return it with the profile view

--- src/main/java/com/example/pathfinder/web/UserProfileController.java
+++ src/main/java/com/example/pathfinder/web/UserProfileController.java
@@ -1,14 +1,30 @@
 package com.example.pathfinder.web;
 
+import com.example.pathfinder.model.view.UserProfileViewModel;
+import com.example.pathfinder.service.UserService;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class UserProfileController {
+  private final UserService userService;
+  private final ModelMapper modelMapper;
+
+  public UserProfileController(UserService userService, ModelMapper modelMapper) {
+    this.userService = userService;
+    this.modelMapper = modelMapper;
+  }
 
   @GetMapping("/users/profile/{id}")
-  public String profilePage(@PathVariable Long id) {
+  public String profilePage(@PathVariable Long id, Model model) {
+
+    UserProfileViewModel userViewModel =
+            this.modelMapper.map(this.userService.findById(id), UserProfileViewModel.class);
+    model.addAttribute("userViewModel", userViewModel);
+
     return "profile";
   }
 
