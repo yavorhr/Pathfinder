69d06d7cda9c427f016c922c0b158699b5ec8f33
wolfie <yavorhristozov@gmail.com>
2024-09-27 14:03:44 +0300

    Update UserService.java
    add findById(Long id)

--- src/main/java/com/example/pathfinder/service/UserService.java
+++ src/main/java/com/example/pathfinder/service/UserService.java
@@ -1,6 +1,7 @@
 package com.example.pathfinder.service;
 
 import com.example.pathfinder.model.service.UserLoginServiceModel;
+import com.example.pathfinder.model.service.UserProfileServiceModel;
 import com.example.pathfinder.model.service.UserRegisterServiceModel;
 
 public interface UserService {
@@ -15,4 +16,6 @@ public interface UserService {
   void loginUser(Long id, String username);
 
   void logout();
+
+  UserProfileServiceModel findById(Long id);
 }
