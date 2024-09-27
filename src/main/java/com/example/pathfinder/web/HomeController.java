94b6d9b32665187477485e2aca2cedb4fd03ed23
wolfie <yavorhristozov@gmail.com>
2024-09-27 15:03:02 +0300

    Feat : Render Portfolio pictures

--- src/main/java/com/example/pathfinder/web/HomeController.java
+++ src/main/java/com/example/pathfinder/web/HomeController.java
@@ -1,13 +1,25 @@
 package com.example.pathfinder.web;
 
+import com.example.pathfinder.service.PictureService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class HomeController {
+  private final PictureService pictureService;
+
+  public HomeController(PictureService pictureService) {
+    this.pictureService = pictureService;
+  }
 
   @GetMapping("/")
-  public String homePage() {
+  public String homePage(Model model) {
+    List<String> urls = this.pictureService.findPictureUrls();
+
+    model.addAttribute("pictures", urls);
     return "index";
   }
 
@@ -15,4 +27,6 @@ public class HomeController {
   public String aboutPage() {
     return "about";
   }
+
+
 }
