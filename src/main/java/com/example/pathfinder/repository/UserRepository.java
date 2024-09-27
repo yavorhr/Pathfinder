350023d36a7e8f4c8005b6f8ae26871c045d669c
wolfie <yavorhristozov@gmail.com>
2024-09-27 12:44:35 +0300

    Update UserRepository.java
    Add findByUsernameAndPassword(String username, String password)

--- src/main/java/com/example/pathfinder/repository/UserRepository.java
+++ src/main/java/com/example/pathfinder/repository/UserRepository.java
@@ -12,4 +12,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
   Optional<User> findUserByUsername(String username);
 
   Optional<User> findByEmail(String email);
+
+  User findByUsernameAndPassword(String username, String password);
 }
