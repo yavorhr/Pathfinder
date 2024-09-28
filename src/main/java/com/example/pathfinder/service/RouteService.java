fe15380e39fc9cbd9213ea731278adc2637844e8
wolfie <yavorhristozov@gmail.com>
2024-09-28 10:40:10 +0300

    Update RouteService.java
    Add <RouteViewModel> findAllRoutes()

--- src/main/java/com/example/pathfinder/service/RouteService.java
+++ src/main/java/com/example/pathfinder/service/RouteService.java
@@ -1,4 +1,9 @@
 package com.example.pathfinder.service;
 
+import com.example.pathfinder.model.service.RouteViewModel;
+
+import java.util.List;
+
 public interface RouteService {
+  List<RouteViewModel> findAllRoutes();
 }
