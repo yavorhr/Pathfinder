b6d433882216b9d9914aa1e6596035dadb0bcd10
wolfie <yavorhristozov@gmail.com>
2024-09-28 11:03:58 +0300

    Update RouteController.java
    Update  @GetMapping("/routes/details/{id}") action which now returns a RouteDetailsViewModel depending on the @PathVariable argument

--- src/main/java/com/example/pathfinder/web/RouteController.java
+++ src/main/java/com/example/pathfinder/web/RouteController.java
@@ -1,11 +1,12 @@
 package com.example.pathfinder.web;
 
-import com.example.pathfinder.model.service.RouteServiceModel;
-import com.example.pathfinder.model.service.RouteViewModel;
+import com.example.pathfinder.model.view.RouteDetailsViewModel;
+import com.example.pathfinder.model.view.RouteViewModel;
 import com.example.pathfinder.service.RouteService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -21,7 +22,16 @@ public class RouteController {
   public String getRoutesPage(Model model) {
     List<RouteViewModel> allRoutes = this.routeService.findAllRoutes();
     model.addAttribute("routes", allRoutes);
+
     return "routes";
+  }
+
+  @GetMapping("/routes/details/{id}")
+  public String getRouteDetailsPage(@PathVariable Long id ,Model model) {
+    RouteDetailsViewModel route = this.routeService.findById(id);
+    model.addAttribute("routeDetails", route);
+
+    return "route-details";
   }
 
 }
