41b4244599f6232d8973b4a0149f58af7916bbb0
wolfie <yavorhristozov@gmail.com>
2024-10-22 17:38:57 +0300

    Add : Custom annotation for Unique email

---
+++ src/main/java/com/example/pathfinder/validation/UniqueEmail.java
@@ -0,0 +1,20 @@
+package com.example.pathfinder.validation;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = UniqueEmailValidator.class)
+public @interface UniqueEmail {
+  String message() default "Email is occupied";
+
+  Class<?>[] groups() default { };
+
+  Class<? extends Payload>[] payload() default { };
+}
---
+++ src/main/java/com/example/pathfinder/validation/UniqueEmailValidator.java
@@ -0,0 +1,21 @@
+package com.example.pathfinder.validation;
+
+import com.example.pathfinder.service.UserService;
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+public class UniqueEmailValidator implements ConstraintValidator<UniqueEmail,String > {
+  private final UserService userService;
+
+  public UniqueEmailValidator(UserService userService) {
+    this.userService = userService;
+  }
+
+  @Override
+  public boolean isValid(String email, ConstraintValidatorContext context) {
+    if (email == null) {
+      return true;
+    }
+    return userService.isEmailAvailable(email);
+  }
+}
