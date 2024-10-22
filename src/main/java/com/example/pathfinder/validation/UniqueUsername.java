9de0b54d589c47a2bd867e4c70245c29930a2fdf
wolfie <yavorhristozov@gmail.com>
2024-10-22 16:35:33 +0300

    Add : UniqueUsername annotation

---
+++ src/main/java/com/example/pathfinder/validation/UniqueUsername.java
@@ -0,0 +1,21 @@
+package com.example.pathfinder.validation;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = UniqueUsernameValidator.class)
+public @interface UniqueUsername {
+
+  String message() default "Username is occupied";
+
+  Class<?>[] groups() default { };
+
+  Class<? extends Payload>[] payload() default { };
+}
