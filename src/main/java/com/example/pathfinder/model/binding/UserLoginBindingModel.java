1a1143fe8aff68f7c88b12a0c319c2fa2c14260a
wolfie <yavorhristozov@gmail.com>
2024-10-23 11:03:58 +0300

    Refact : UserLoginBindingModel
    Remove jakarta validations as they are not needed
    Add @ValidLogin custom annotation for checking if login is sucessful

--- src/main/java/com/example/pathfinder/model/binding/UserLoginBindingModel.java
+++ src/main/java/com/example/pathfinder/model/binding/UserLoginBindingModel.java
@@ -1,34 +1,32 @@
 package com.example.pathfinder.model.binding;
 
+import com.example.pathfinder.validation.ValidLogin;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 
+@ValidLogin
 public class UserLoginBindingModel {
-  private String password;
   private String username;
+  private String password;
 
   public UserLoginBindingModel() {
   }
 
-  @NotNull
-  @NotEmpty
-  public String getPassword() {
-    return password;
-  }
-
-  @NotNull
-  @NotEmpty
   public String getUsername() {
     return username;
   }
 
-  public UserLoginBindingModel setPassword(String password) {
-    this.password = password;
-    return this;
+  public String getPassword() {
+    return password;
   }
 
   public UserLoginBindingModel setUsername(String username) {
     this.username = username;
+    return this;
+  }
+
+  public UserLoginBindingModel setPassword(String password) {
+    this.password = password;
     return this;
   }
 }
