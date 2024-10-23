e432b0cab61bd4ff06bf73d1341ba23ba9137463
wolfie <yavorhristozov@gmail.com>
2024-10-23 11:06:20 +0300

    Update CurrentUser.java
    Extends CurrentUser fields

--- src/main/java/com/example/pathfinder/util/CurrentUser.java
+++ src/main/java/com/example/pathfinder/util/CurrentUser.java
@@ -1,19 +1,50 @@
 package com.example.pathfinder.util;
 
+import com.example.pathfinder.model.entity.User;
+import com.example.pathfinder.model.entity.enums.UserRoleEnum;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.annotation.SessionScope;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Component
 @SessionScope
 public class CurrentUser {
   private Long id;
   private String username;
+  private boolean isLoggedIn;
+  private String fullName;
+  private String email;
+  private Set<UserRoleEnum> roles;
 
   public CurrentUser() {
+    this.isLoggedIn = false;
+    this.roles = new HashSet<>();
+  }
+
+  public Set<UserRoleEnum> getRoles() {
+    return roles;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public String getFullName() {
+    return fullName;
   }
 
   public String getUsername() {
     return username;
+  }
+
+  public boolean isLoggedIn() {
+    return this.id != null;
+  }
+
+  public Long getId() {
+    return id;
   }
 
   public CurrentUser setUsername(String username) {
@@ -21,18 +52,22 @@ public class CurrentUser {
     return this;
   }
 
-  public Long getId() {
-    return id;
-  }
-
   public CurrentUser setId(Long id) {
     this.id = id;
+    return this;
+  }
+
+  public CurrentUser setLoggedIn(boolean loggedIn) {
+    isLoggedIn = loggedIn;
     return this;
   }
 
   public void clear() {
     this.id = null;
     this.username = "";
+    this.email = "";
+    this.fullName = "";
+    this.isLoggedIn = false;
   }
 
   public void login(Long id, String username) {
@@ -40,7 +75,33 @@ public class CurrentUser {
     this.setId(id);
   }
 
-  public boolean isLoggedIn(){
-    return this.id != null;
+  public CurrentUser setFullName(String fullName) {
+    this.fullName = fullName;
+    return this;
+  }
+
+  public CurrentUser setEmail(String email) {
+    this.email = email;
+    return this;
+  }
+
+  public CurrentUser setRoles(Set<UserRoleEnum> roles) {
+    this.roles = roles;
+    return this;
+  }
+
+  private void addRole(UserRoleEnum roleEnum) {
+    this.roles.add(roleEnum);
+
+  }
+
+  public void saveUserToSession(User loggedInUserEntity) {
+    this.setUsername(loggedInUserEntity.getUsername());
+    this.setLoggedIn(true);
+    this.setFullName(loggedInUserEntity.getFullName());
+    this.setId(loggedInUserEntity.getId());
+    this.setEmail(loggedInUserEntity.getEmail());
+
+    loggedInUserEntity.getRoles().forEach(r -> this.addRole(r.getRole()));
   }
 }
