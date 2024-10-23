701edccf44beed742b038375f6ae70112edf7ba1
wolfie <yavorhristozov@gmail.com>
2024-10-23 11:07:26 +0300

    Update UserLoginServiceModel.java
    Add password field

--- src/main/java/com/example/pathfinder/model/service/UserLoginServiceModel.java
+++ src/main/java/com/example/pathfinder/model/service/UserLoginServiceModel.java
@@ -3,8 +3,13 @@ package com.example.pathfinder.model.service;
 public class UserLoginServiceModel {
   private Long id;
   private String username;
+  private String password;
 
   public UserLoginServiceModel() {
+  }
+
+  public String getPassword() {
+    return password;
   }
 
   public Long getId() {
@@ -22,6 +27,11 @@ public class UserLoginServiceModel {
 
   public UserLoginServiceModel setUsername(String username) {
     this.username = username;
+    return this;
+  }
+
+  public UserLoginServiceModel setPassword(String password) {
+    this.password = password;
     return this;
   }
 }
