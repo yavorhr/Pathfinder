02f555e983c0a14637a40165e35910808ca1b8e4
wolfie <yavorhristozov@gmail.com>
2024-10-24 17:40:06 +0300

    Update RouteRepository.java
    Add  findByName(String name);

--- src/main/java/com/example/pathfinder/repository/RouteRepository.java
+++ src/main/java/com/example/pathfinder/repository/RouteRepository.java
@@ -4,6 +4,10 @@ import com.example.pathfinder.model.entity.Route;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface RouteRepository extends JpaRepository<Route,Long> {
+
+  Optional<Route> findByName(String name);
 }
