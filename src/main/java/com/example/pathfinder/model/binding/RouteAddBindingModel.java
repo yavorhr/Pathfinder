161c9a0d131ce292195daeced118339890ea5a2b
wolfie <yavorhristozov@gmail.com>
2024-10-24 17:41:53 +0300

    Update RouteAddBindingModel.java

--- src/main/java/com/example/pathfinder/model/binding/RouteAddBindingModel.java
+++ src/main/java/com/example/pathfinder/model/binding/RouteAddBindingModel.java
@@ -2,91 +2,91 @@ package com.example.pathfinder.model.binding;
 
 import com.example.pathfinder.model.entity.enums.CategoryEnum;
 import com.example.pathfinder.model.entity.enums.LevelEnum;
+import com.example.pathfinder.validation.route.UniqueRouteName;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
 import org.springframework.web.multipart.MultipartFile;
-
 import java.util.Set;
 
 public class RouteAddBindingModel {
-    private String name;
-    private String description;
-    private MultipartFile gpxCoordinates;
-    private LevelEnum level;
-    private String videoUrl;
-    private Set<CategoryEnum> categories;
-    private Integer distance;
+  private String name;
+  private String description;
+  private MultipartFile gpxCoordinates;
+  private LevelEnum level;
+  private String videoUrl;
+  private Set<CategoryEnum> categories;
+  private Integer distance;
 
-    public RouteAddBindingModel() {
-    }
+  public RouteAddBindingModel() {
+  }
 
-    @NotNull
-    @Positive
-    public Integer getDistance() {
-        return distance;
-    }
+  @NotNull
+  @Positive
+  public Integer getDistance() {
+    return distance;
+  }
 
-    @Size(min = 3, max = 20, message = "Name must be between 3 and 20 charters")
-    public String getName() {
-        return name;
-    }
+  @UniqueRouteName
+  @Size(min = 3, max = 20, message = "Name must be between 3 and 20 charters")
+  public String getName() {
+    return name;
+  }
 
-    @Size(min = 3)
-    public String getDescription() {
-        return description;
-    }
+  @Size(min = 3)
+  public String getDescription() {
+    return description;
+  }
 
-    public MultipartFile getGpxCoordinates() {
-        return gpxCoordinates;
-    }
+  public MultipartFile getGpxCoordinates() {
+    return gpxCoordinates;
+  }
 
-    @NotNull
-    public Set<CategoryEnum> getCategories() {
-        return categories;
-    }
+  public Set<CategoryEnum> getCategories() {
+    return categories;
+  }
 
-    @NotNull
-    public LevelEnum getLevel() {
-        return level;
-    }
+  @NotNull
+  public LevelEnum getLevel() {
+    return level;
+  }
 
-    public String getVideoUrl() {
-        return videoUrl;
-    }
+  public String getVideoUrl() {
+    return videoUrl;
+  }
 
-    public RouteAddBindingModel setName(String name) {
-        this.name = name;
-        return this;
-    }
+  public RouteAddBindingModel setName(String name) {
+    this.name = name;
+    return this;
+  }
 
-    public RouteAddBindingModel setLevel(LevelEnum level) {
-        this.level = level;
-        return this;
-    }
+  public RouteAddBindingModel setLevel(LevelEnum level) {
+    this.level = level;
+    return this;
+  }
 
-    public RouteAddBindingModel setVideoUrl(String videoUrl) {
-        this.videoUrl = videoUrl;
-        return this;
-    }
+  public RouteAddBindingModel setVideoUrl(String videoUrl) {
+    this.videoUrl = videoUrl;
+    return this;
+  }
 
-    public RouteAddBindingModel setCategories(Set<CategoryEnum> categories) {
-        this.categories = categories;
-        return this;
-    }
+  public RouteAddBindingModel setCategories(Set<CategoryEnum> categories) {
+    this.categories = categories;
+    return this;
+  }
 
-    public RouteAddBindingModel setGpxCoordinates(MultipartFile gpxCoordinates) {
-        this.gpxCoordinates = gpxCoordinates;
-        return this;
-    }
+  public RouteAddBindingModel setGpxCoordinates(MultipartFile gpxCoordinates) {
+    this.gpxCoordinates = gpxCoordinates;
+    return this;
+  }
 
-    public RouteAddBindingModel setDescription(String description) {
-        this.description = description;
-        return this;
-    }
+  public RouteAddBindingModel setDescription(String description) {
+    this.description = description;
+    return this;
+  }
 
-    public RouteAddBindingModel setDistance(Integer distance) {
-        this.distance = distance;
-        return this;
-    }
+  public RouteAddBindingModel setDistance(Integer distance) {
+    this.distance = distance;
+    return this;
+  }
 }
