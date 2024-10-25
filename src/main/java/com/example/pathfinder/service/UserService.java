42f4c16dc6441fa13c19d79f83a1aa19c5231829
wolfie <yavorhristozov@gmail.com>
2024-10-25 12:55:30 +0300

    Update UserService.java
    rename findById() to findUserServiceById()

--- src/main/java/com/example/pathfinder/service/UserService.java
+++ src/main/java/com/example/pathfinder/service/UserService.java
@@ -1,20 +1,25 @@
 package com.example.pathfinder.service;
 
+import com.example.pathfinder.model.entity.User;
 import com.example.pathfinder.model.service.UserLoginServiceModel;
 import com.example.pathfinder.model.service.UserProfileServiceModel;
 import com.example.pathfinder.model.service.UserRegisterServiceModel;
 
+import java.util.Optional;
+
 public interface UserService {
-  
+
   void registerUser(UserRegisterServiceModel map);
 
   void logout();
 
-  UserProfileServiceModel findById(Long id);
+  UserProfileServiceModel findUserServiceById(Long id);
 
   boolean isUserNameAvailable(String username);
 
   boolean isEmailAvailable(String email);
 
   boolean login(UserLoginServiceModel serviceModel);
+
+  Optional<User> findById(Long id);
 }
