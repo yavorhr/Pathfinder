1b1b37e61ab38bc520b0fe90971776090875ed7e
wolfie <yavorhristozov@gmail.com>
2024-10-25 13:38:40 +0300

    Update RouteServiceImpl.java
    Finish addNewRoute() method

--- src/main/java/com/example/pathfinder/service/impl/RouteServiceImpl.java
+++ src/main/java/com/example/pathfinder/service/impl/RouteServiceImpl.java
@@ -1,24 +1,35 @@
 package com.example.pathfinder.service.impl;
 
+import com.example.pathfinder.model.entity.Category;
+import com.example.pathfinder.model.entity.Route;
+import com.example.pathfinder.model.entity.enums.CategoryEnum;
 import com.example.pathfinder.model.service.AddRouteServiceModel;
 import com.example.pathfinder.model.view.RouteDetailsViewModel;
 import com.example.pathfinder.model.view.RouteViewModel;
 import com.example.pathfinder.repository.RouteRepository;
+import com.example.pathfinder.service.CategoryService;
 import com.example.pathfinder.service.RouteService;
+import com.example.pathfinder.service.UserService;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 @Service
 public class RouteServiceImpl implements RouteService {
   private final RouteRepository routeRepository;
   private final ModelMapper modelMapper;
+  private final UserService userService;
+  private final CategoryService categoryService;
 
-  public RouteServiceImpl(RouteRepository repository, ModelMapper modelMapper) {
+  public RouteServiceImpl(RouteRepository repository, ModelMapper modelMapper, UserService userService, CategoryService categoryService) {
     this.routeRepository = repository;
     this.modelMapper = modelMapper;
+    this.userService = userService;
+    this.categoryService = categoryService;
   }
 
   @Override
@@ -39,17 +50,36 @@ public class RouteServiceImpl implements RouteService {
   public RouteDetailsViewModel findById(Long id) {
     return routeRepository
             .findById(id)
-            .map(route -> modelMapper.map(route,RouteDetailsViewModel.class))
+            .map(route -> modelMapper.map(route, RouteDetailsViewModel.class))
             .orElse(null);
   }
 
   @Override
   public Long addNewRoute(AddRouteServiceModel routeServiceModel) {
-    return null;
+    Route route = this.modelMapper.map(routeServiceModel, Route.class);
+
+    route.setAuthor(this.userService.findById(routeServiceModel.getAuthorId()).get());
+    route.setCategories(mapCategories(routeServiceModel.getCategories()));
+
+    route = this.routeRepository.save(route);
+    
+    return route.getId();
   }
 
   @Override
   public boolean findRouteByName(String name) {
     return this.routeRepository.findByName(name).isEmpty();
+  }
+
+  // Helpers
+  private Set<Category> mapCategories(Set<CategoryEnum> categoryEnums) {
+    Set<Category> categoriesSet = new HashSet<>();
+
+    categoryEnums.forEach(catEnum -> {
+      Category entity = this.categoryService.findByName(catEnum).get();
+      categoriesSet.add(entity);
+    });
+
+    return categoriesSet;
   }
 }
