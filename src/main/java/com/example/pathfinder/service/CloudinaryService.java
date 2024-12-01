0ba75b821d1e1ebe099893206c03035edd0c9113
wolfie <yavorhristozov@gmail.com>
2024-12-01 12:03:13 +0200

    Add : CloudinaryServce impl
    - Implementation of the Cloudinary API
    - Override the upload and delete methods

---
+++ src/main/java/com/example/pathfinder/service/CloudinaryService.java
@@ -0,0 +1,13 @@
+package com.example.pathfinder.service;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+public interface CloudinaryService {
+
+  CloudinaryImage upload(MultipartFile file) throws IOException;
+
+  boolean delete(String publicId);
+
+}
