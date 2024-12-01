ce1c97a5ad45fd51b504d7c33f094cc80865b2c6
wolfie <yavorhristozov@gmail.com>
2024-12-01 16:34:31 +0200

    Update : PictureServiceImpl
    Add addPicture() method in the service layer

--- src/main/java/com/example/pathfinder/service/PictureService.java
+++ src/main/java/com/example/pathfinder/service/PictureService.java
@@ -1,7 +1,11 @@
 package com.example.pathfinder.service;
 
+import com.example.pathfinder.model.service.PictureAddServiceModel;
+
 import java.util.List;
 
 public interface PictureService {
   List<String> findPictureUrls();
+
+  void addPicture(PictureAddServiceModel uploaded);
 }
--- src/main/java/com/example/pathfinder/service/impl/PictureServiceImpl.java
+++ src/main/java/com/example/pathfinder/service/impl/PictureServiceImpl.java
@@ -1,7 +1,10 @@
 package com.example.pathfinder.service.impl;
 
+import com.example.pathfinder.model.entity.Picture;
+import com.example.pathfinder.model.service.PictureAddServiceModel;
 import com.example.pathfinder.repository.PictureRepository;
 import com.example.pathfinder.service.PictureService;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -9,13 +12,21 @@ import java.util.List;
 @Service
 public class PictureServiceImpl implements PictureService {
   private final PictureRepository pictureRepository;
+  private final ModelMapper modelMapper;
 
-  public PictureServiceImpl(PictureRepository pictureRepository) {
+  public PictureServiceImpl(PictureRepository pictureRepository, ModelMapper modelMapper) {
     this.pictureRepository = pictureRepository;
+    this.modelMapper = modelMapper;
   }
 
   @Override
   public List<String> findPictureUrls() {
     return this.pictureRepository.getAllPictureUrls();
+  }
+
+  @Override
+  public void addPicture(PictureAddServiceModel serviceModel) {
+    Picture picture = this.pictureRepository.save(this.modelMapper.map(serviceModel, Picture.class));
+    System.out.println();
   }
 }
