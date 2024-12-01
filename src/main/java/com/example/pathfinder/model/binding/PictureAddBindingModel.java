505c338df15f59bae618a55e2657a34adcd2c03e
wolfie <yavorhristozov@gmail.com>
2024-12-01 16:33:31 +0200

    Add : PictureAddBindingModel

--- src/main/java/com/example/pathfinder/model/binding/PictureAddBindingModel.java
+++ src/main/java/com/example/pathfinder/model/binding/PictureAddBindingModel.java
@@ -2,26 +2,35 @@ package com.example.pathfinder.model.binding;
 
 import org.springframework.web.multipart.MultipartFile;
 
-public class PictureBindingModel {
+public class PictureAddBindingModel {
 
   private String title;
   private MultipartFile picture;
+  private Long routeId;
 
   public String getTitle() {
     return title;
   }
 
-  public PictureBindingModel setTitle(String title) {
+  public PictureAddBindingModel setTitle(String title) {
     this.title = title;
     return this;
   }
 
+  public Long getRouteId() {
+    return routeId;
+  }
   public MultipartFile getPicture() {
     return picture;
   }
 
-  public PictureBindingModel setPicture(MultipartFile picture) {
+  public PictureAddBindingModel setPicture(MultipartFile picture) {
     this.picture = picture;
+    return this;
+  }
+
+  public PictureAddBindingModel setRouteId(Long routeId) {
+    this.routeId = routeId;
     return this;
   }
 }
