4ca79ccaa2c8c0a645f75077f4a2b621de99a2cd
wolfie <yavorhristozov@gmail.com>
2024-12-02 10:35:04 +0200

    Update PictureServiceImpl.java
    persist picture to the database

--- src/main/java/com/example/pathfinder/service/impl/PictureServiceImpl.java
+++ src/main/java/com/example/pathfinder/service/impl/PictureServiceImpl.java
@@ -4,6 +4,8 @@ import com.example.pathfinder.model.entity.Picture;
 import com.example.pathfinder.model.service.PictureAddServiceModel;
 import com.example.pathfinder.repository.PictureRepository;
 import com.example.pathfinder.service.PictureService;
+import com.example.pathfinder.service.RouteService;
+import com.example.pathfinder.service.UserService;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
@@ -11,10 +13,14 @@ import java.util.List;
 
 @Service
 public class PictureServiceImpl implements PictureService {
+  private final UserService userService;
+  private final RouteService routeService;
   private final PictureRepository pictureRepository;
   private final ModelMapper modelMapper;
 
-  public PictureServiceImpl(PictureRepository pictureRepository, ModelMapper modelMapper) {
+  public PictureServiceImpl(UserService userService, RouteService routeService, PictureRepository pictureRepository, ModelMapper modelMapper) {
+    this.userService = userService;
+    this.routeService = routeService;
     this.pictureRepository = pictureRepository;
     this.modelMapper = modelMapper;
   }
@@ -25,8 +31,22 @@ public class PictureServiceImpl implements PictureService {
   }
 
   @Override
-  public void addPicture(PictureAddServiceModel serviceModel) {
-    Picture picture = this.pictureRepository.save(this.modelMapper.map(serviceModel, Picture.class));
-    System.out.println();
+  public void addPicture(PictureAddServiceModel pictureAddServiceModel) {
+    Picture picture = mapToPicture(pictureAddServiceModel);
+
+    this.pictureRepository.save(picture);
+  }
+
+  // Helpers
+  private Picture mapToPicture(PictureAddServiceModel pictureAddServiceModel) {
+    Picture picture = new Picture();
+
+    picture.setUrl(pictureAddServiceModel.getUrl());
+    picture.setPublicId(pictureAddServiceModel.getPublicId());
+    picture.setAuthor(this.userService.findById(pictureAddServiceModel.getAuthorId()).get());
+    picture.setTitle(pictureAddServiceModel.getTitle());
+    picture.setRoute(this.routeService.findById(pictureAddServiceModel.getRouteId()).get());
+
+    return picture;
   }
 }
