1d522b7dc9660cf8e2addd94e75be2f8368b6e43
wolfie <yavorhristozov@gmail.com>
2024-12-13 16:59:24 +0200

    Update pom.xml
    Add @Bean PasswordEncoder

---
+++ src/main/java/com/example/pathfinder/config/ApplicationSecurityConfiguration.java
@@ -0,0 +1,64 @@
+package com.example.pathfinder.config;
+
+import com.example.pathfinder.model.entity.enums.UserRoleEnum;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableMethodSecurity(prePostEnabled = true)
+public class ApplicationSecurityConfiguration {
+  private final UserDetailsService userDetailsService;
+  private final PasswordEncoder passwordEncoder;
+
+  public ApplicationSecurityConfiguration(UserDetailsService userDetailsService, PasswordEncoder passwordEncoder) {
+    this.userDetailsService = userDetailsService;
+    this.passwordEncoder = passwordEncoder;
+  }
+
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    http.authorizeHttpRequests(authz ->
+            authz.
+                    requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll().
+                    requestMatchers(EndpointRequest.toAnyEndpoint()).permitAll().
+                    requestMatchers("/", "/users/login", "/users/register", "/logger/**").permitAll().
+                    requestMatchers("/**").authenticated())
+            .formLogin(form -> form.
+                    loginPage("/users/login").
+                    usernameParameter("email").
+                    passwordParameter(UsernamePasswordAuthenticationFilter.SPRING_SECURITY_FORM_PASSWORD_KEY).
+                    defaultSuccessUrl("/", true).
+                    failureForwardUrl("/users/login-error"))
+            .logout(logout -> logout.
+                    logoutUrl("/users/logout").
+                    logoutSuccessUrl("/").
+                    invalidateHttpSession(true).
+                    deleteCookies("JSESSIONID"))
+            .securityContext(securityContext ->
+                    securityContext.requireExplicitSave(false));
+
+    return http.build();
+  }
+
+  @Bean
+  public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
+    AuthenticationManagerBuilder auth =
+            http.getSharedObject(AuthenticationManagerBuilder.class);
+
+    auth
+            .userDetailsService(userDetailsService)
+            .passwordEncoder(passwordEncoder);
+
+    return auth.build();
+  }
+}
