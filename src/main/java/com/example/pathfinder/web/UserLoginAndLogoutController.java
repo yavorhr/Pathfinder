9a59fb01de82887da66800b674db046f72d1ea9e
wolfie <yavorhristozov@gmail.com>
2024-12-13 18:14:10 +0200

    Update UserLoginAndLogoutController.java

--- src/main/java/com/example/pathfinder/web/UserLoginAndLogoutController.java
+++ src/main/java/com/example/pathfinder/web/UserLoginAndLogoutController.java
@@ -1,28 +1,16 @@
 package com.example.pathfinder.web;
 
-import com.example.pathfinder.model.binding.UserLoginBindingModel;
-import com.example.pathfinder.model.service.UserLoginServiceModel;
-import com.example.pathfinder.service.UserService;
-import jakarta.servlet.http.HttpSession;
-import jakarta.validation.Valid;
-import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
 
 @Controller
 @RequestMapping("/users")
 public class UserLoginAndLogoutController {
-  private final UserService userService;
-  private final ModelMapper modelMapper;
 
-  public UserLoginAndLogoutController(UserService userService, ModelMapper modelMapper) {
-    this.userService = userService;
-    this.modelMapper = modelMapper;
+  public UserLoginAndLogoutController() {
   }
 
   @ModelAttribute("userLoginBindingModel")
@@ -35,27 +23,4 @@ public class UserLoginAndLogoutController {
     return "login";
   }
 
-  @PostMapping("/login")
-  public String loginConfirm(@Valid UserLoginBindingModel userLoginBindingModel,
-                             BindingResult bindingResult,
-                             RedirectAttributes redirectAttributes) {
-
-    if (bindingResult.hasErrors()) {
-      redirectAttributes
-              .addFlashAttribute("userLoginBindingModel", userLoginBindingModel)
-              .addFlashAttribute("org.springframework.validation.BindingResult.userLoginBindingModel", bindingResult);
-      return "redirect:login";
-    }
-
-    this.userService.login(this.modelMapper.map(userLoginBindingModel, UserLoginServiceModel.class));
-
-    return "index";
-  }
-
-  @GetMapping("/logout")
-  public String logout(HttpSession httpSession) {
-//    httpSession.invalidate();
-    this.userService.logout();
-    return "redirect:/";
-  }
 }
