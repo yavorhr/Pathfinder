a12f419d6b6df8464c0e929d0e592c2e3305e39c
wolfie <yavorhristozov@gmail.com>
2024-12-13 16:59:43 +0200

    Update ApplicationBeanConfiguration.java

--- src/main/java/com/example/pathfinder/config/ApplicationBeanConfiguration.java
+++ src/main/java/com/example/pathfinder/config/ApplicationBeanConfiguration.java
@@ -3,6 +3,8 @@ package com.example.pathfinder.config;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
 
 @Configuration
 public class ApplicationBeanConfiguration {
@@ -10,5 +12,10 @@ public class ApplicationBeanConfiguration {
   @Bean
   public ModelMapper modelMapper(){
     return new ModelMapper();
+  }
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return Pbkdf2PasswordEncoder.defaultsForSpringSecurity_v5_8();
   }
 }
