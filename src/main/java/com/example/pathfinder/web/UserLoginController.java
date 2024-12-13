bbc8592c84924ad992ee21b1df8a7b07bd41f3bd
wolfie <yavorhristozov@gmail.com>
2024-12-13 18:39:24 +0200

    Update UserLoginController.java
    Add  @PostMapping("/users/login-error") - when login is not successful this action method will be triggered by Spring security filter chain

--- src/main/java/com/example/pathfinder/web/UserLoginController.java
+++ src/main/java/com/example/pathfinder/web/UserLoginController.java
@@ -1,18 +1,29 @@
 package com.example.pathfinder.web;
 
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/users")
 public class UserLoginController {
 
-  public UserLoginController() {
+  @GetMapping("/login")
+  public String login() {
+    return "login";
   }
 
-  @GetMapping("/login")
-  public String loginPage() {
-    return "login";
+  @PostMapping("/users/login-error")
+  public String failedLogin(@ModelAttribute(UsernamePasswordAuthenticationFilter.SPRING_SECURITY_FORM_USERNAME_KEY) String username,
+                            RedirectAttributes attributes) {
+
+    attributes.addFlashAttribute("bad_credentials", true);
+    attributes.addFlashAttribute("username", username);
+
+    return "redirect:/users/login";
   }
 }
