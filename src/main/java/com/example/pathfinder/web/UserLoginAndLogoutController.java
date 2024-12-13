be87cb4683b2dd6e9e805b656e3035350c6654d8
wolfie <yavorhristozov@gmail.com>
2024-12-13 18:15:20 +0200

    Update UserLoginAndLogoutController.java
    remove @ModelAttribute("userLoginBindingModel")

--- src/main/java/com/example/pathfinder/web/UserLoginAndLogoutController.java
+++ src/main/java/com/example/pathfinder/web/UserLoginAndLogoutController.java
@@ -2,7 +2,6 @@ package com.example.pathfinder.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
@@ -13,14 +12,8 @@ public class UserLoginAndLogoutController {
   public UserLoginAndLogoutController() {
   }
 
-  @ModelAttribute("userLoginBindingModel")
-  public UserLoginBindingModel userLoginBindingModel() {
-    return new UserLoginBindingModel();
-  }
-
   @GetMapping("/login")
   public String loginPage() {
     return "login";
   }
-
 }
