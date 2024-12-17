68a869796704119623081208041ea654aa43d65e
wolfie <yavorhristozov@gmail.com>
2024-12-17 14:39:16 +0200

    Update AdminController.java
    Add @PostMapping("/admin/remove-user/{email}")
    
    The method removes user by passed email from the client

--- src/main/java/com/example/pathfinder/web/AdminController.java
+++ src/main/java/com/example/pathfinder/web/AdminController.java
@@ -1,21 +1,35 @@
 package com.example.pathfinder.web;
 
 import com.example.pathfinder.service.AdminNotificationService;
+import com.example.pathfinder.service.UserService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class AdminController {
   private final AdminNotificationService notificationService;
+  private final UserService userService;
+  private final AdminNotificationService adminNotificationService;
 
-  public AdminController(AdminNotificationService notificationService) {
+  public AdminController(AdminNotificationService notificationService, UserService userService, AdminNotificationService adminNotificationService) {
     this.notificationService = notificationService;
+    this.userService = userService;
+    this.adminNotificationService = adminNotificationService;
   }
 
   @GetMapping("/admin/notifications")
   public String viewNotifications(Model model) {
     model.addAttribute("notifications", notificationService.getNotifications());
     return "notifications";
+  }
+
+  @PostMapping("/admin/remove-user/{email}")
+  @ResponseBody
+  public ResponseEntity<String> deleteUserById(@PathVariable String email) {
+    this.userService.deleteUser(email);
+    this.notificationService.deleteNotification(email);
+    return ResponseEntity.ok("User with email" + email + " has been deleted.");
   }
 }
