a0e8c62f3267ce1223cceba48021b2e9dfc80ee8
wolfie <yavorhristozov@gmail.com>
2024-12-17 13:05:07 +0200

    Update UserRegisteredEvent.java
    update constructor parameters

--- src/main/java/com/example/pathfinder/service/events/UserRegisteredEvent.java
+++ src/main/java/com/example/pathfinder/service/events/UserRegisteredEvent.java
@@ -2,20 +2,16 @@ package com.example.pathfinder.service.events;
 
 import org.springframework.context.ApplicationEvent;
 
+import java.time.LocalDateTime;
+
 public class UserRegisteredEvent extends ApplicationEvent {
-  private final Long userId;
   private final String username;
   private final String email;
 
-  public UserRegisteredEvent(Object source, Long userId, String username, String email) {
+  public UserRegisteredEvent(Object source, LocalDateTime registeredOn, String username, String email) {
     super(source);
-    this.userId = userId;
     this.username = username;
     this.email = email;
-  }
-
-  public Long getUserId() {
-    return userId;
   }
 
   public String getUsername() {
