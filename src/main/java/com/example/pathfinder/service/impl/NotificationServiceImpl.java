a856e686a8ded8f027290a00ee0c11ce35e7013e
wolfie <yavorhristozov@gmail.com>
2024-12-17 13:05:13 +0200

    Delete NotificationServiceImpl.java

--- src/main/java/com/example/pathfinder/service/impl/NotificationServiceImpl.java
+++
@@ -1,8 +0,0 @@
-package com.example.pathfinder.service.impl;
-
-import com.example.pathfinder.service.NotificationService;
-import org.springframework.stereotype.Service;
-
-@Service
-public class NotificationServiceImpl implements NotificationService {
-}
