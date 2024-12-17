0a55fe5db99813db2cac866e28bd0257c3e4511d
wolfie <yavorhristozov@gmail.com>
2024-12-17 13:51:11 +0200

    Update NotificationRepository.java
    delete notification by user's email

--- src/main/java/com/example/pathfinder/repository/NotificationRepository.java
+++ src/main/java/com/example/pathfinder/repository/NotificationRepository.java
@@ -6,4 +6,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface NotificationRepository extends JpaRepository<Notification,Long> {
+
+  void deleteByEmail(String email);
 }
