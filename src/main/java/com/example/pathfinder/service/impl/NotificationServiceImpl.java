781f9cb6b5b84ae0a29f0378d6dd97275e026f09
wolfie <yavorhristozov@gmail.com>
2024-12-17 11:15:56 +0200

    Add : NotificationService class as a service implementation
    - Created NotificationServiceImpl class to implement NotificationService interface
    - Annotated with @Service for Spring's service layer component scanning

---
+++ src/main/java/com/example/pathfinder/service/impl/NotificationServiceImpl.java
@@ -0,0 +1,8 @@
+package com.example.pathfinder.service.impl;
+
+import com.example.pathfinder.service.NotificationService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class NotificationServiceImpl implements NotificationService {
+}
