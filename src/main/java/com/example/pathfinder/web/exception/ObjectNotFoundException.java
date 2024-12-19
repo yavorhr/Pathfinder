57ca1d29219e24f8509aee0a3d8332f10350dddd
wolfie <yavorhristozov@gmail.com>
2024-12-19 09:58:18 +0200

    Create ObjectNotFoundException.java
    Add ObjectNotFoundException class

---
+++ src/main/java/com/example/pathfinder/web/exception/ObjectNotFoundException.java
@@ -0,0 +1,8 @@
+package com.example.pathfinder.web.exception;
+
+public class ObjectNotFoundException extends RuntimeException {
+
+  public ObjectNotFoundException(String message) {
+    super(message);
+  }
+}
