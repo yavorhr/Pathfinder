83b854f4ffb7bec26a14a6ad8460b100a17b6fb9
wolfie <yavorhristozov@gmail.com>
2024-12-19 16:53:47 +0200

    Add : createComment(Service model) service

--- src/main/java/com/example/pathfinder/service/CommentService.java
+++ src/main/java/com/example/pathfinder/service/CommentService.java
@@ -1,9 +1,12 @@
 package com.example.pathfinder.service;
 
+import com.example.pathfinder.model.service.CommentServiceModel;
 import com.example.pathfinder.model.view.CommentViewModel;
 
 import java.util.List;
 
 public interface CommentService {
   List<CommentViewModel> getAllComments(Long routeId);
+
+  CommentViewModel createComment(CommentServiceModel serviceModel);
 }
--- src/main/java/com/example/pathfinder/service/impl/CommentServiceImpl.java
+++ src/main/java/com/example/pathfinder/service/impl/CommentServiceImpl.java
@@ -1,17 +1,19 @@
 package com.example.pathfinder.service.impl;
 
 import com.example.pathfinder.model.entity.Comment;
+import com.example.pathfinder.model.service.CommentServiceModel;
 import com.example.pathfinder.model.view.CommentViewModel;
 import com.example.pathfinder.repository.CommentRepository;
 import com.example.pathfinder.service.CommentService;
 import com.example.pathfinder.service.RouteService;
+import com.example.pathfinder.service.UserService;
 import com.example.pathfinder.web.exception.ObjectNotFoundException;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
 import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 @Service
@@ -19,11 +21,14 @@ public class CommentServiceImpl implements CommentService {
   private final CommentRepository commentRepository;
   private final RouteService routeService;
   private final ModelMapper modelMapper;
+  private final UserService userService;
 
-  public CommentServiceImpl(CommentRepository commentRepository, RouteService routeService, ModelMapper modelMapper) {
+
+  public CommentServiceImpl(CommentRepository commentRepository, RouteService routeService, ModelMapper modelMapper, UserService userService) {
     this.commentRepository = commentRepository;
     this.routeService = routeService;
     this.modelMapper = modelMapper;
+    this.userService = userService;
   }
 
   @Override
@@ -41,6 +46,31 @@ public class CommentServiceImpl implements CommentService {
             .getComments()
             .stream().map(this::mapToViewComment)
             .collect(Collectors.toList());
+  }
+
+  @Override
+  public CommentViewModel createComment(CommentServiceModel serviceModel) {
+    //TODO - add NullPointerException Handler
+    Objects.requireNonNull(serviceModel.getCreatorEmail());
+
+    var route = routeService.
+            findById(serviceModel.getRouteId()).
+            orElseThrow(() -> new ObjectNotFoundException("Route with id " + serviceModel.getRouteId() + " not found!"));
+
+    var author = userService.
+            findByEmail(serviceModel.getCreatorEmail()).
+            orElseThrow(() -> new ObjectNotFoundException("User with email " + serviceModel.getCreatorEmail() + " not found!"));
+
+    Comment newComment = new Comment();
+
+    newComment.setApproved(true);
+    newComment.setTextContent(serviceModel.getMessage());
+    newComment.setRoute(route);
+    newComment.setAuthor(author);
+
+    Comment savedComment = commentRepository.save(newComment);
+
+    return mapToViewComment(savedComment);
   }
 
   private CommentViewModel mapToViewComment(Comment commentEntity) {
