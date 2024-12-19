0964d87b8c4fd36c26e22328daf97e094b5b514e
wolfie <yavorhristozov@gmail.com>
2024-12-19 16:55:39 +0200

    Create ApiError.java
    POJO Java object, which is returned to the client if the server throws error (invalid comment length)
    The object will persist the error message and it will be used to be rendered on the client (with JS)

---
+++ src/main/java/com/example/pathfinder/validation/ApiError.java
@@ -0,0 +1,33 @@
+package com.example.pathfinder.validation;
+
+import org.springframework.http.HttpStatus;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ApiError {
+
+  private final HttpStatus status;
+  private List<String> fieldWithErrors = new ArrayList<>();
+
+  public ApiError(HttpStatus status) {
+    this.status = status;
+  }
+
+  public HttpStatus getStatus() {
+    return status;
+  }
+
+  public void addFieldWithError(String error) {
+    fieldWithErrors.add(error);
+  }
+
+  public List<String> getFieldWithErrors() {
+    return fieldWithErrors;
+  }
+
+  public ApiError setFieldWithErrors(List<String> fieldWithErrors) {
+    this.fieldWithErrors = fieldWithErrors;
+    return this;
+  }
+}
