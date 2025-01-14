829516abef99d46599bea7d740a35f99efe657c1
wolfie <yavorhristozov@gmail.com>
2025-01-14 14:26:29 +0200

    Update NewCommentBindingModel.java
    Add Long parentCommentId - will be used to obtain the parent_comment_id, if such exists

--- src/main/java/com/example/pathfinder/model/binding/NewCommentBindingModel.java
+++ src/main/java/com/example/pathfinder/model/binding/NewCommentBindingModel.java
@@ -5,6 +5,7 @@ import jakarta.validation.constraints.Size;
 
 public class NewCommentBindingModel {
   private String message;
+  private Long parentCommentId;
 
   public NewCommentBindingModel setMessage(String message) {
     this.message = message;
@@ -16,4 +17,15 @@ public class NewCommentBindingModel {
   public String getMessage() {
     return message;
   }
+
+  public Long getParentCommentId() {
+    return parentCommentId;
+  }
+
+  public NewCommentBindingModel setParentCommentId(Long parentCommentId) {
+    this.parentCommentId = parentCommentId;
+    return this;
+  }
+
+
 }
