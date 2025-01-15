44460d06292a640bb372f9e473d6fe795658f70e
wolfie <yavorhristozov@gmail.com>
2025-01-15 13:41:20 +0200

    Update toggleAdminPanel.js
    add additional checks if elements exist.
    If user is not admin, the dropdown will not be rendered, which will lead to null elements in the querySelectors

--- src/main/resources/static/js/navigation/toggleAdminPanel.js
+++ src/main/resources/static/js/navigation/toggleAdminPanel.js
@@ -1,16 +1,19 @@
 document.addEventListener("DOMContentLoaded", function () {
     const dropdownToggle = document.querySelector(".dropdown");
     const dropdownMenu = document.querySelector(".dropdown-menu");
-    const menuItems = dropdownMenu.querySelectorAll(".dropdown-item");
 
-    dropdownToggle.addEventListener("click", function (event) {
-        event.preventDefault();
+    const menuItems = dropdownMenu ? dropdownMenu.querySelectorAll(".dropdown-item") : [];
 
-        dropdownMenu.classList.toggle("show");
-    });
+    if (dropdownToggle) {
+        dropdownToggle.addEventListener("click", function (event) {
+            event.preventDefault();
+
+            dropdownMenu.classList.toggle("show");
+        });
+    }
 
     document.addEventListener("click", function (event) {
-        if (!dropdownToggle.contains(event.target) && !dropdownMenu.contains(event.target)) {
+        if (dropdownToggle && !dropdownToggle.contains(event.target) && !dropdownMenu.contains(event.target)) {
             dropdownMenu.classList.remove("show");
         }
     });
@@ -21,6 +24,7 @@ document.addEventListener("DOMContentLoaded", function () {
             event.stopPropagation();
         });
     });
+
 });
 
 
