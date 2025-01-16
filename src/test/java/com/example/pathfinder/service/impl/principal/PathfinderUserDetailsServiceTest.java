e6205364e76ec18342087156214733da84476fc8
wolfie <yavorhristozov@gmail.com>
2025-01-16 13:25:50 +0200

    Update PathfinderUserDetailsServiceTest.java
    Add test for none existing user -  void testUserNotFound()

--- src/test/java/com/example/pathfinder/service/impl/principal/PathfinderUserDetailsServiceTest.java
+++ src/test/java/com/example/pathfinder/service/impl/principal/PathfinderUserDetailsServiceTest.java
@@ -4,16 +4,15 @@ import com.example.pathfinder.model.entity.UserEntity;
 import com.example.pathfinder.model.entity.UserRoleEntity;
 import com.example.pathfinder.model.entity.enums.UserRoleEnum;
 import com.example.pathfinder.repository.UserRepository;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-
-import javax.management.relation.Role;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 import java.util.Set;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(MockitoExtension.class)
 class PathfinderUserDetailsServiceTest {
@@ -26,13 +25,14 @@ class PathfinderUserDetailsServiceTest {
   private UserRepository mockUserRepository;
 
   @BeforeEach
-  void init(){
+  void init() {
+    //ARRANGE
     serviceToTest = new PathfinderUserDetailsService(mockUserRepository);
 
     adminRole = new UserRoleEntity();
     adminRole.setRole(UserRoleEnum.ADMIN);
 
-    adminRole = new UserRoleEntity();
+    userRole = new UserRoleEntity();
     userRole.setRole(UserRoleEnum.USER);
 
     testUser = new UserEntity();
@@ -40,5 +40,13 @@ class PathfinderUserDetailsServiceTest {
     testUser.setEmail("lucho@lucho.com");
     testUser.setPassword("topsecret");
     testUser.setRoles(Set.of(adminRole, userRole));
+  }
+
+  @Test
+  void testUserNotFound() {
+    Assertions.assertThrows(
+            UsernameNotFoundException.class,
+            () -> serviceToTest.loadUserByUsername("invalid_user_email@not-exist.com")
+    );
   }
 }
