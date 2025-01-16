314844d94945d190d0120b4dfdb20d93b2d55f6d
wolfie <yavorhristozov@gmail.com>
2025-01-16 14:18:10 +0200

    Update PathfinderUserDetailsServiceTest.java
    test testPathfinderUserFistConstructor()

--- src/test/java/com/example/pathfinder/service/impl/principal/PathfinderUserDetailsServiceTest.java
+++ src/test/java/com/example/pathfinder/service/impl/principal/PathfinderUserDetailsServiceTest.java
@@ -4,7 +4,6 @@ import com.example.pathfinder.model.entity.UserEntity;
 import com.example.pathfinder.model.entity.UserRoleEntity;
 import com.example.pathfinder.model.entity.enums.UserRoleEnum;
 import com.example.pathfinder.repository.UserRepository;
-import org.junit.Assert;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -13,9 +12,10 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-
+import java.util.Collection;
 import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -32,7 +32,7 @@ class PathfinderUserDetailsServiceTest {
 
   @BeforeEach
   void init() {
-    //ARRANGE
+    //Arrange before each test
     serviceToTest = new PathfinderUserDetailsService(mockUserRepository);
 
     adminRole = new UserRoleEntity();
@@ -42,14 +42,12 @@ class PathfinderUserDetailsServiceTest {
     userRole.setRole(UserRoleEnum.USER);
 
     testUser = new UserEntity();
-
     testUser.setId(1L);
     testUser.setEnabled(true);
     testUser.setUsername("lucho");
     testUser.setEmail("lucho@lucho.com");
     testUser.setPassword("topsecret");
     testUser.setRoles(Set.of(adminRole, userRole));
-
   }
 
   @Test
@@ -96,10 +94,38 @@ class PathfinderUserDetailsServiceTest {
     Assertions.assertEquals(1L, ((PathfinderUser) userDetails).getId());
     Assertions.assertEquals(testUser.getEmail(), userDetails.getUsername());
     Assertions.assertEquals(testUser.getPassword(), userDetails.getPassword());
-    Assertions.assertTrue(userDetails.isEnabled()); 
+    Assertions.assertTrue(userDetails.isEnabled());
     Assertions.assertTrue(userDetails.isAccountNonExpired());
     Assertions.assertTrue(userDetails.isCredentialsNonExpired());
     Assertions.assertTrue(userDetails.isAccountNonLocked());
     Assertions.assertEquals(2, userDetails.getAuthorities().size());
   }
+
+  // test on first ctor of PathfinderUser
+  @Test
+  void testPathfinderUserFistConstructor() {
+    // Arrange
+    Collection<SimpleGrantedAuthority> authorities = Set.of(
+            new SimpleGrantedAuthority("ROLE_USER"),
+            new SimpleGrantedAuthority("ROLE_ADMIN")
+    );
+
+    // Act
+    PathfinderUser pathfinderUser = new PathfinderUser(
+            testUser.getId(),
+            testUser.getUsername(),
+            testUser.getPassword(),
+            authorities);
+
+    // Assert
+    Assertions. assertEquals(testUser.getId(), pathfinderUser.getId());
+    Assertions.assertEquals(testUser.getUsername(), pathfinderUser.getUsername());
+    Assertions.assertEquals(testUser.getPassword(), pathfinderUser.getPassword());
+    Assertions.assertTrue(pathfinderUser.isEnabled());
+    Assertions.assertTrue(pathfinderUser.isAccountNonExpired());
+    Assertions.assertTrue(pathfinderUser.isCredentialsNonExpired());
+    Assertions.assertTrue(pathfinderUser.isAccountNonLocked());
+    Assertions.assertEquals(2, pathfinderUser.getAuthorities().size());
+  }
+
 }
