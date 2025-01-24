5dd11e11e30ebacd7870e8871379c2de360d42d5
wolfie <yavorhristozov@gmail.com>
2025-01-24 18:03:55 +0200

    Update UserProfileController.java

--- src/main/java/com/example/pathfinder/web/UserProfileController.java
+++ src/main/java/com/example/pathfinder/web/UserProfileController.java
@@ -17,7 +17,6 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-
 import java.io.IOException;
 import java.util.Map;
 import java.util.stream.Collectors;
