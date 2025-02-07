86315c0c8ad09ee153cd6fd00272835de3e1ee90
wolfie <yavorhristozov@gmail.com>
2025-02-07 15:59:38 +0200

    Update ProfilePicture.java

--- src/main/java/com/example/pathfinder/model/entity/ProfilePicture.java
+++ src/main/java/com/example/pathfinder/model/entity/ProfilePicture.java
@@ -7,26 +7,26 @@ import jakarta.persistence.*;
 public class ProfilePicture extends BaseEntity {
   private String publicId;
   private String url;
-//  private UserEntity user;
+  private UserEntity user;
 
   public ProfilePicture() {
   }
 
-  @Column(name = "public_id", unique = true, columnDefinition = "VARCHAR(255) DEFAULT 'pathfinder/users-pictures/xv9rvzhomv0a1yojubw3'")
+  @Column(name = "public_id_number",  columnDefinition = "VARCHAR(255) DEFAULT 'pathfinder/users-pictures/xv9rvzhomv0a1yojubw3'")
   public String getPublicId() {
     return publicId;
   }
 
-  @Column(name = "url", unique = true, columnDefinition = "https://res.cloudinary.com/yavorhr/image/upload/v1738929875/pathfinder/users-pictures/profile_m3npdq.jpg'")
+  @Column(name = "url",  columnDefinition = "VARCHAR(255) DEFAULT 'https://res.cloudinary.com/yavorhr/image/upload/v1738929875/pathfinder/users-pictures/profile_m3npdq.jpg'")
   public String getUrl() {
     return url;
   }
 
-//  @OneToOne
-//  @JoinColumn(name = "user_id", referencedColumnName = "id", nullable = false, unique = true)
-//  public UserEntity getUser() {
-//    return user;
-//  }
+  @OneToOne
+  @JoinColumn(name = "user_id", referencedColumnName = "id", nullable = false)
+  public UserEntity getUser() {
+    return user;
+  }
 
   public ProfilePicture setPublicId(String publicId) {
     this.publicId = publicId;
@@ -38,8 +38,8 @@ public class ProfilePicture extends BaseEntity {
     return this;
   }
 
-//  public ProfilePicture setUser(UserEntity userEntity) {
-//    this.user = userEntity;
-//    return this;
-//  }
+  public ProfilePicture setUser(UserEntity userEntity) {
+    this.user = userEntity;
+    return this;
+  }
 }
