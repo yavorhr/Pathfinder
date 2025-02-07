941032d8f421c86bfe13cae5614216f961e6737e
wolfie <yavorhristozov@gmail.com>
2025-02-07 13:28:26 +0200

    Update RouteController.java

--- src/main/java/com/example/pathfinder/web/RouteController.java
+++ src/main/java/com/example/pathfinder/web/RouteController.java
@@ -15,7 +15,6 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
 import java.io.IOException;
 import java.util.List;
 
