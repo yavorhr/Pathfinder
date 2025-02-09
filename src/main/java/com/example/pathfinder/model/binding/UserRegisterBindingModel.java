bebd30a21d0b7e6ad67dd6829b8fd599fb97709a
wolfie <yavorhristozov@gmail.com>
2025-02-09 13:59:19 +0200

    Update UserRegisterBindingModel.java
    add additional properties

--- src/main/java/com/example/pathfinder/model/binding/UserRegisterBindingModel.java
+++ src/main/java/com/example/pathfinder/model/binding/UserRegisterBindingModel.java
@@ -3,38 +3,53 @@ package com.example.pathfinder.model.binding;
 import com.example.pathfinder.validation.register.DoesPasswordAndConfirmPasswordMatch;
 import com.example.pathfinder.validation.register.UniqueEmail;
 import com.example.pathfinder.validation.register.UniqueUsername;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Positive;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 
 @DoesPasswordAndConfirmPasswordMatch
 public class UserRegisterBindingModel {
   private String username;
-  private String fullName;
+  private String firstName;
+  private String lastName;
+  private String gender;
   private String email;
   private Integer age;
   private String password;
   private String confirmPassword;
+  private String aboutMe;
+  private String facebookAcc;
+  private String instagramAcc;
+  private String linkedIn;
 
   public UserRegisterBindingModel() {
   }
 
   @UniqueUsername
   @NotNull
-  @Size(min = 4,max = 20, message = "Username must be between 4 and 20 symbols")
+  @Size(min = 4, max = 20, message = "Username must be between 4 and 20 symbols")
   public String getUsername() {
     return username;
   }
 
   @NotNull
-  @Size(min = 3, max = 30)
-  public String getFullName() {
-    return fullName;
+  @Size(min = 3, max = 20, message = "Fist name must be between 3 and 20 symbols")
+  public String getFirstName() {
+    return this.firstName;
+  }
+
+  @NotNull
+  @Size(min = 3, max = 20, message = "Last name must be between 3 and 20 symbols")
+  public String getLastName() {
+    return this.lastName;
+  }
+
+  @NotNull
+  @NotEmpty
+  public String getGender() {
+    return gender;
   }
 
   @UniqueEmail
-  @Email(message ="PLease insert valid Email")
+  @Email(message = "PLease insert valid Email")
   @NotNull
   public String getEmail() {
     return email;
@@ -47,11 +62,10 @@ public class UserRegisterBindingModel {
   }
 
   @NotNull
-  @Size(min = 5,max = 20)
+  @Size(min = 5, max = 20)
   public String getPassword() {
     return password;
   }
-
 
   @NotNull
   @Size(min = 5, max = 20)
@@ -64,9 +78,25 @@ public class UserRegisterBindingModel {
     return this;
   }
 
-  public UserRegisterBindingModel setFullName(String fullName) {
-    this.fullName = fullName;
+  public UserRegisterBindingModel setGender(String gender) {
+    this.gender = gender;
     return this;
+  }
+
+  public String getAboutMe() {
+    return aboutMe;
+  }
+
+  public String getFacebookAcc() {
+    return facebookAcc;
+  }
+
+  public String getInstagramAcc() {
+    return instagramAcc;
+  }
+
+  public String getLinkedIn() {
+    return linkedIn;
   }
 
   public UserRegisterBindingModel setEmail(String email) {
@@ -88,4 +118,35 @@ public class UserRegisterBindingModel {
     this.confirmPassword = confirmPassword;
     return this;
   }
+
+  public UserRegisterBindingModel setAboutMe(String aboutMe) {
+    this.aboutMe = aboutMe;
+    return this;
+  }
+
+  public UserRegisterBindingModel setFacebookAcc(String facebookAcc) {
+    this.facebookAcc = facebookAcc;
+    return this;
+  }
+
+  public UserRegisterBindingModel setInstagramAcc(String instagramAcc) {
+    this.instagramAcc = instagramAcc;
+    return this;
+  }
+
+  public UserRegisterBindingModel setLinkedIn(String linkedIn) {
+    this.linkedIn = linkedIn;
+    return this;
+  }
+
+  public UserRegisterBindingModel setFirstName(String firstName) {
+    this.firstName = firstName;
+    return this;
+  }
+
+  public UserRegisterBindingModel setLastName(String lastName) {
+    this.lastName = lastName;
+    return this;
+  }
+
 }
