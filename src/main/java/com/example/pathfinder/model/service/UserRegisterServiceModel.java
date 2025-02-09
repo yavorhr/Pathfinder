71a5e2c419021cb96b27f734884c9315d7f322ea
wolfie <yavorhristozov@gmail.com>
2025-02-09 13:59:09 +0200

    Update UserRegisterServiceModel.java
    add gender, firstName and lastName props

--- src/main/java/com/example/pathfinder/model/service/UserRegisterServiceModel.java
+++ src/main/java/com/example/pathfinder/model/service/UserRegisterServiceModel.java
@@ -1,18 +1,19 @@
 package com.example.pathfinder.model.service;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Positive;
-import jakarta.validation.constraints.Size;
-
 public class UserRegisterServiceModel {
   private String username;
   private String email;
-  private String fullName;
+  private String firstName;
+  private String lastName;
+  private String gender;
   private Integer age;
   private String password;
 
   public UserRegisterServiceModel() {
+  }
+
+  public String getGender() {
+    return gender;
   }
 
   public String getUsername() {
@@ -23,8 +24,12 @@ public class UserRegisterServiceModel {
     return email;
   }
 
-  public String getFullName() {
-    return fullName;
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public String getLastName() {
+    return lastName;
   }
 
   public Integer getAge() {
@@ -45,8 +50,18 @@ public class UserRegisterServiceModel {
     return this;
   }
 
-  public UserRegisterServiceModel setFullName(String fullName) {
-    this.fullName = fullName;
+  public UserRegisterServiceModel setFirstName(String firstName) {
+    this.firstName = firstName;
+    return this;
+  }
+
+  public UserRegisterServiceModel setLastName(String lastName) {
+    this.lastName = lastName;
+    return this;
+  }
+
+  public UserRegisterServiceModel setGender(String gender) {
+    this.gender = gender;
     return this;
   }
 
