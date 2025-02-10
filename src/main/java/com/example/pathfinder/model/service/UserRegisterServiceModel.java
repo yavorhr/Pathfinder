6bd8b5073ee8eb2643b52e7f77c359e6804a6b92
wolfie <yavorhristozov@gmail.com>
2025-02-10 15:14:24 +0200

    Update UserRegisterServiceModel.java

--- src/main/java/com/example/pathfinder/model/service/UserRegisterServiceModel.java
+++ src/main/java/com/example/pathfinder/model/service/UserRegisterServiceModel.java
@@ -8,6 +8,10 @@ public class UserRegisterServiceModel {
   private String gender;
   private Integer age;
   private String password;
+  private String aboutMe;
+  private String facebookAcc;
+  private String instagramAcc;
+  private String linkedIn;
 
   public UserRegisterServiceModel() {
   }
@@ -38,6 +42,22 @@ public class UserRegisterServiceModel {
 
   public String getPassword() {
     return password;
+  }
+
+  public String getAboutMe() {
+    return aboutMe;
+  }
+
+  public String getFacebookAcc() {
+    return facebookAcc;
+  }
+
+  public String getInstagramAcc() {
+    return instagramAcc;
+  }
+
+  public String getLinkedIn() {
+    return linkedIn;
   }
 
   public UserRegisterServiceModel setUsername(String username) {
@@ -72,6 +92,26 @@ public class UserRegisterServiceModel {
 
   public UserRegisterServiceModel setPassword(String password) {
     this.password = password;
+    return this;
+  }
+
+  public UserRegisterServiceModel setAboutMe(String aboutMe) {
+    this.aboutMe = aboutMe;
+    return this;
+  }
+
+  public UserRegisterServiceModel setFacebookAcc(String facebookAcc) {
+    this.facebookAcc = facebookAcc;
+    return this;
+  }
+
+  public UserRegisterServiceModel setInstagramAcc(String instagramAcc) {
+    this.instagramAcc = instagramAcc;
+    return this;
+  }
+
+  public UserRegisterServiceModel setLinkedIn(String linkedIn) {
+    this.linkedIn = linkedIn;
     return this;
   }
 }
