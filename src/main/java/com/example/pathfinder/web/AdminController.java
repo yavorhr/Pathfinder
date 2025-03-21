945a3355009aa57839c5c42e36ff385279598ff3
wolfie <yavorhristozov@gmail.com>
2025-03-21 12:23:40 +0200

    Update AdminController.java
    remvoe AdminNotificationService dependency

--- src/main/java/com/example/pathfinder/web/AdminController.java
+++ src/main/java/com/example/pathfinder/web/AdminController.java
@@ -1,29 +1,23 @@
 package com.example.pathfinder.web;
 
 import com.example.pathfinder.model.common.UserUpdateStatusResponse;
-import com.example.pathfinder.service.AdminNotificationService;
 import com.example.pathfinder.service.UserService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
-import java.util.Map;
-
 @Controller
 public class AdminController {
-  private final AdminNotificationService notificationService;
   private final UserService userService;
 
-  public AdminController(AdminNotificationService notificationService, UserService userService) {
-    this.notificationService = notificationService;
+  public AdminController( UserService userService) {
     this.userService = userService;
   }
 
   @GetMapping("/admin/notifications")
   public String viewNotifications(Model model) {
-    model.addAttribute("notifications", notificationService.getNotifications());
+
     return "notifications";
   }
 
@@ -31,7 +25,7 @@ public class AdminController {
   @ResponseBody
   public ResponseEntity<String> deleteUserById(@PathVariable String email) {
     this.userService.deleteUser(email);
-    this.notificationService.deleteNotification(email);
+//    this.notificationService.deleteNotification(email);
     return ResponseEntity.ok("User with email" + email + " has been deleted.");
   }
 
