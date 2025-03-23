0f506ec7b8c5d7be04c3f24ca8b23a301683719a
wolfie <yavorhristozov@gmail.com>
2025-03-23 14:51:38 +0200

    Update notifications.js
    enable user bug fix

--- src/main/resources/static/js/admin/notifications.js
+++ src/main/resources/static/js/admin/notifications.js
@@ -1,76 +1,75 @@
 document.addEventListener("DOMContentLoaded", function () {
     const deleteButtons = document.querySelectorAll(".delete-btn");
     const enableButtons = document.querySelectorAll(".enable-btn");
-    const userEmail = document.querySelector("#email").textContent.trim();
+    const userEmail = document.querySelector(".email").textContent.trim();
 
     const csrfToken = document.querySelector('meta[name="_csrf"]').getAttribute('content');
     const csrfHeader = document.querySelector('meta[name="_csrf_header"]').getAttribute('content');
 
     // Change role
-    document.querySelectorAll(".change-role-btn").forEach(button => {
-        button.addEventListener("click", function () {
-            const row = this.closest("tr");
-            const roleSelect = row.querySelector(".role-select");
-            const selectedRole = roleSelect.value;
-
-            if (!selectedRole) {
-                alert("Please select a role before changing.");
-                return;
-            }
-
-            fetch("/admin/change-role", {
-                method: "POST",
-                headers: {"Content-Type": "application/json"},
-                body: JSON.stringify({email: userEmail, role: selectedRole})
-            })
-                .then(response => response.json())
-                .then(data => {
-                    alert(data.message || "Role updated successfully!");
-                })
-                .catch(error => {
-                    console.error("Error updating role:", error);
-                    alert("Failed to update role.");
-                });
-        });
-    });
+    // document.querySelectorAll(".change-role-btn").forEach(button => {
+    //     button.addEventListener("click", function () {
+    //         const row = this.closest("tr");
+    //         const roleSelect = row.querySelector(".role-select");
+    //         const selectedRole = roleSelect.value;
+    //
+    //         if (!selectedRole) {
+    //             alert("Please select a role before changing.");
+    //             return;
+    //         }
+    //
+    //         fetch("/admin/change-role", {
+    //             method: "POST",
+    //             headers: {"Content-Type": "application/json"},
+    //             body: JSON.stringify({email: userEmail, role: selectedRole})
+    //         })
+    //             .then(response => response.json())
+    //             .then(data => {
+    //                 alert(data.message || "Role updated successfully!");
+    //             })
+    //             .catch(error => {
+    //                 console.error("Error updating role:", error);
+    //                 alert("Failed to update role.");
+    //             });
+    //     });
+    // });
 
     // Delete user by email
-    deleteButtons.forEach(button => {
-        button.addEventListener("click", (event) => {
-
-            const emailElement = event.target.closest("tr").querySelector("#email");
-            const email = emailElement.textContent;
-
-            fetch(`/admin/remove-user/${encodeURIComponent(email)}`, {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                    [csrfHeader]: csrfToken
-                }
-            })
-                .then(response => {
-                    if (response.ok) {
-                        alert("User deleted successfully.");
-                        window.location.reload(); // Optionally refresh the page
-                    } else {
-                        alert("Error deleting user.");
-                    }
-                })
-                .catch(error => {
-                    console.error("Error:", error);
-                    alert("An unexpected error occurred.");
-                });
-
-
-        });
-    });
+    // deleteButtons.forEach(button => {
+    //     button.addEventListener("click", (event) => {
+    //
+    //         const emailElement = event.target.closest("tr").querySelector("#email");
+    //         const email = emailElement.textContent;
+    //
+    //         fetch(`/admin/remove-user/${encodeURIComponent(email)}`, {
+    //             method: "POST",
+    //             headers: {
+    //                 "Content-Type": "application/json",
+    //                 [csrfHeader]: csrfToken
+    //             }
+    //         })
+    //             .then(response => {
+    //                 if (response.ok) {
+    //                     alert("User deleted successfully.");
+    //                     window.location.reload(); // Optionally refresh the page
+    //                 } else {
+    //                     alert("Error deleting user.");
+    //                 }
+    //             })
+    //             .catch(error => {
+    //                 console.error("Error:", error);
+    //                 alert("An unexpected error occurred.");
+    //             });
+    //     });
+    // });
 
     // Enable user
     enableButtons.forEach(b => {
         b.addEventListener("click", (e) => {
             const clickedButton = e.currentTarget;
+
             const tr = clickedButton.closest("tr");
-            const username = tr.querySelector('td').textContent.trim();
+            const username = tr.querySelector('.username').textContent.trim();
 
             fetch(`/admin/change-user-access/${username}`, {
                 method: "PUT",
@@ -87,11 +86,18 @@ document.addEventListener("DOMContentLoaded", function () {
                 .then(data => {
                         console.log(data)
                         const isEnabled = data.enabled;
+                        const status = tr.querySelector(".status");
 
                         if (isEnabled) {
-                            clickedButton.textContent = "Disable User";
+                            status.classList.add("enable-user-access");
+                            status.classList.remove("disable-user-access");
+                            status.textContent = "Active"
+                            clickedButton.textContent = "Disable user"
                         } else {
-                            clickedButton.textContent = "Enable User";
+                            status.classList.remove("enable-user-access");
+                            status.classList.add("disable-user-access");
+                            status.textContent = "Not active"
+                            clickedButton.textContent = "Enable user"
                         }
                     }
                 )
