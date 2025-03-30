8806e8eab403ec7f09e824835f0b38891a6a4f73
wolfie <yavorhristozov@gmail.com>
2025-03-30 16:05:39 +0300

    Update PasswordsValidator.java
    remove not used code

--- src/main/java/com/example/pathfinder/validation/register/PasswordsValidator.java
+++ src/main/java/com/example/pathfinder/validation/register/PasswordsValidator.java
@@ -1,20 +1,12 @@
 package com.example.pathfinder.validation.register;
 
 import com.example.pathfinder.model.binding.UserRegisterBindingModel;
-import com.example.pathfinder.model.entity.UserEntity;
-import com.example.pathfinder.service.UserService;
-import com.example.pathfinder.web.exception.ObjectNotFoundException;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
-import org.modelmapper.ModelMapper;
 
 public class PasswordsValidator implements ConstraintValidator<DoesPasswordAndConfirmPasswordMatch, UserRegisterBindingModel> {
-  private final UserService userService;
-  private final ModelMapper modelMapper;
 
-  public PasswordsValidator(UserService userService, ModelMapper modelMapper) {
-    this.userService = userService;
-    this.modelMapper = modelMapper;
+  public PasswordsValidator() {
   }
 
   @Override
@@ -22,6 +14,7 @@ public class PasswordsValidator implements ConstraintValidator<DoesPasswordAndCo
     if (bindingModel.getEmail().isEmpty()) {
       return false;
     }
+
     return bindingModel.getPassword().equals(bindingModel.getConfirmPassword());
   }
 }
