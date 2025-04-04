5072ec6143f9b2496fcb32fc154060959f78ec42
wolfie <yavorhristozov@gmail.com>
2025-04-04 15:15:55 +0300

    Update PathfinderApplication.java
    add @EnableScheduling

--- src/main/java/com/example/pathfinder/PathfinderApplication.java
+++ src/main/java/com/example/pathfinder/PathfinderApplication.java
@@ -2,7 +2,10 @@ package com.example.pathfinder;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 
+@EnableScheduling
 @SpringBootApplication
 public class PathfinderApplication {
 
