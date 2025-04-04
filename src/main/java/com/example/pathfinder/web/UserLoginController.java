df5912cfc9c381acc042cbcaaeaf5962afa04bdb
wolfie <yavorhristozov@gmail.com>
2025-04-04 15:13:11 +0300

    Update UserLoginController.java
    add queryParam "locked" to /users/login for showing message to the user if the account is locked

--- src/main/java/com/example/pathfinder/web/UserLoginController.java
+++ src/main/java/com/example/pathfinder/web/UserLoginController.java
@@ -2,10 +2,8 @@ package com.example.pathfinder.web;
 
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
@@ -13,7 +11,10 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 public class UserLoginController {
 
   @GetMapping("/login")
-  public String login() {
+  public String login(@RequestParam(value = "locked", required = false) String locked, Model model) {
+    if (locked != null && locked.equals("true")){
+      model.addAttribute("locked", true);
+    }
     return "login";
   }
 
