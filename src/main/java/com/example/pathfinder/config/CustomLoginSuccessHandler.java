847a0bd586026825fe86be97f26664acd52e26dd
wolfie <yavorhristozov@gmail.com>
2025-04-04 14:40:22 +0300

    Create CustomLoginSuccessHandler.java
    Custom handler implementation of user successful login

---
+++ src/main/java/com/example/pathfinder/config/CustomLoginSuccessHandler.java
@@ -0,0 +1,33 @@
+package com.example.pathfinder.config;
+
+import com.example.pathfinder.model.entity.UserEntity;
+import com.example.pathfinder.service.UserService;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class CustomLoginSuccessHandler implements AuthenticationSuccessHandler {
+  private final UserService userService;
+
+  public CustomLoginSuccessHandler(UserService userService) {
+    this.userService = userService;
+  }
+
+  @Override
+  public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+    String username = authentication.getName();
+    UserEntity user = userService.findByEmail(username);
+
+    if (user != null) {
+      userService.resetFailedAttempts(user);
+    }
+
+    response.sendRedirect("/");
+  }
+}
