e153683244c4bb9987b1cb3a203b30661ade1753
wolfie <yavorhristozov@gmail.com>
2025-04-29 14:57:28 +0300

    Create LoginErrorType.java
    add enum for login error types

---
+++ src/main/java/com/example/pathfinder/model/entity/enums/LoginErrorType.java
@@ -0,0 +1,9 @@
+package com.example.pathfinder.model.entity.enums;
+
+public enum LoginErrorType {
+  INVALID_CREDENTIALS,
+  ACCOUNT_LOCKED,
+  ACCOUNT_DISABLED,
+  ACCOUNT_EXPIRED,
+  USER_NOT_FOUND
+}
