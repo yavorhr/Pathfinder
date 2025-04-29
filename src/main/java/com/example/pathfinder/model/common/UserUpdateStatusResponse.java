c8748f99a62c8ed3477d860c5c2152ac4d6ec79f
wolfie <yavorhristozov@gmail.com>
2025-04-29 16:57:51 +0300

    Update UserUpdateStatusResponse.java
    add prop disabledTime to response

--- src/main/java/com/example/pathfinder/model/common/UserUpdateStatusResponse.java
+++ src/main/java/com/example/pathfinder/model/common/UserUpdateStatusResponse.java
@@ -2,14 +2,20 @@ package com.example.pathfinder.model.common;
 
 import com.example.pathfinder.model.entity.enums.UserRoleEnum;
 
+import java.time.LocalDateTime;
 import java.util.Set;
 
 public class UserUpdateStatusResponse {
   private String email;
   private boolean isEnabled;
   private Set<UserRoleEnum> roles;
+  private LocalDateTime disabledTime;
 
   public UserUpdateStatusResponse() {
+  }
+
+  public LocalDateTime getDisabledTime() {
+    return disabledTime;
   }
 
   public String getEmail() {
@@ -36,6 +42,11 @@ public class UserUpdateStatusResponse {
 
   public UserUpdateStatusResponse setRoles(Set<UserRoleEnum> roles) {
     this.roles = roles;
+    return this;
+  }
+
+  public UserUpdateStatusResponse setDisabledTime(LocalDateTime disabledTime) {
+    this.disabledTime = disabledTime;
     return this;
   }
 }
