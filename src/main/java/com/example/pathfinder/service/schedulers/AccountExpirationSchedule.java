699e44ab727474ec6196b61896cabd8efb586ee0
wolfie <yavorhristozov@gmail.com>
2025-04-30 16:59:27 +0300

    Create AccountExpirationSchedule.java
    add AccountExpirationSchedule - if users last login is 1 year ago, the scheduler will make the account expired and the user will be not able to log in

---
+++ src/main/java/com/example/pathfinder/service/schedulers/AccountExpirationSchedule.java
@@ -0,0 +1,32 @@
+package com.example.pathfinder.service.schedulers;
+
+import com.example.pathfinder.model.entity.UserEntity;
+import com.example.pathfinder.service.UserService;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Component
+public class AccountExpirationSchedule {
+
+  private final UserService userService;
+
+  public AccountExpirationSchedule(UserService userService) {
+    this.userService = userService;
+  }
+
+  @Scheduled(cron = "0 0 2 * * *") // Every day at 2 AM
+  public void expireInactiveAccounts() {
+
+    LocalDateTime oneYearAgo = LocalDateTime.now().minusYears(1);
+    List<UserEntity> inactiveUsers = userService.findInactiveUsersSince(oneYearAgo);
+
+    for (UserEntity user : inactiveUsers) {
+      user.setAccountExpired(true);
+      userService.saveUpdatedUser(user);
+    }
+  }
+
+}
