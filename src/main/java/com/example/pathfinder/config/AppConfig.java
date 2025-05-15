febfe1ab295417818ab542f09da6d3f373635fef
wolfie <yavorhristozov@gmail.com>
2025-05-15 16:25:56 +0300

    Update AppConfig.java
    add redis @beans components

--- src/main/java/com/example/pathfinder/config/AppConfig.java
+++ src/main/java/com/example/pathfinder/config/AppConfig.java
@@ -3,8 +3,10 @@ package com.example.pathfinder.config;
 import com.cloudinary.Cloudinary;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-
 import java.util.Map;
 
 @Configuration
@@ -26,5 +28,22 @@ public class AppConfig {
             "api_secret", config.getApiSecret()
         )
     );
+  }
+
+  @Bean
+  public LettuceConnectionFactory redisConnectionFactory(
+          RedisStandaloneConfiguration cfg) {
+    return new LettuceConnectionFactory(cfg);
+  }
+
+  @Bean
+  public RedisStandaloneConfiguration redisStandaloneConfiguration() {
+    return new RedisStandaloneConfiguration("localhost", 6379);
+  }
+
+  @Bean
+  public StringRedisTemplate stringRedisTemplate(
+          LettuceConnectionFactory connectionFactory) {
+    return new StringRedisTemplate(connectionFactory);
   }
 }
