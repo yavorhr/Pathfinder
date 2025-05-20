5ebad1a79728a41c7d4404189538dcf6064b43be
wolfie <yavorhristozov@gmail.com>
2025-05-20 14:28:43 +0300

    Update uploadImage.js

--- src/main/resources/static/js/routeDetails/uploadImage.js
+++ src/main/resources/static/js/routeDetails/uploadImage.js
@@ -1,21 +1,30 @@
 document.addEventListener("DOMContentLoaded", () => {
     const fileInput = document.getElementById("picture-upload");
     const form = document.getElementById("picture-upload-form");
+    const uploadSuccess = document.getElementById("uploadSuccess");
+    const titleInput = document.getElementById("title");
 
     fileInput.addEventListener("change", () => {
         if (fileInput.files.length > 0) {
-            const fileName = fileInput.files[0].name.split(".")[0];
-            const titleInput = document.getElementById("title");
+            const file = fileInput.files[0];
+
             if (titleInput && !titleInput.value) {
+                const fileName = file.name.split(".")[0];
                 titleInput.value = fileName;
+            }
+
+            setTimeout(() => {
+                form.submit();
+            }, 200);
+
+            // Show success message
+            if (uploadSuccess) {
+                uploadSuccess.style.opacity = 1;
+
+                setTimeout(() => {
+                    uploadSuccess.style.opacity = 0;
+                }, 2000);
             }
         }
     });
-
-    fileInput.addEventListener("change", () => {
-        if (fileInput.files.length > 0) {
-            form.submit();
-        }
-    });
-
 });
