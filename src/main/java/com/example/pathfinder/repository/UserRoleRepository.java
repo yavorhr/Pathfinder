c425cf41ad499f40b02a204d967f0ec3a18903e0
wolfie <yavorhristozov@gmail.com>
2025-07-28 17:43:13 +0300

    refact : findByRole(roleEnum)
    refact the repository method to return Optional, so exceptions can be handled in the service layer

--- src/main/java/com/example/pathfinder/repository/UserRoleRepository.java
+++ src/main/java/com/example/pathfinder/repository/UserRoleRepository.java
@@ -5,7 +5,9 @@ import com.example.pathfinder.model.entity.enums.UserRoleEnum;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRoleRepository extends JpaRepository<UserRoleEntity, Long> {
-  UserRoleEntity findByRole(UserRoleEnum role);
+  Optional<UserRoleEntity> findByRole(UserRoleEnum role);
 }
--- src/main/java/com/example/pathfinder/service/impl/UserRolesServiceImpl.java
+++ src/main/java/com/example/pathfinder/service/impl/UserRolesServiceImpl.java
@@ -4,6 +4,7 @@ import com.example.pathfinder.model.entity.UserRoleEntity;
 import com.example.pathfinder.model.entity.enums.UserRoleEnum;
 import com.example.pathfinder.repository.UserRoleRepository;
 import com.example.pathfinder.service.UserRolesService;
+import com.example.pathfinder.web.exception.ObjectNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -16,6 +17,7 @@ public class UserRolesServiceImpl implements UserRolesService {
 
   @Override
   public UserRoleEntity findRoleEntityByRoleEnum(UserRoleEnum roleEnum) {
-    return this.userRoleRepository.findByRole(roleEnum);
+    return this.userRoleRepository.findByRole(roleEnum)
+            .orElseThrow(() -> new ObjectNotFoundException("Role with name " + roleEnum + " was not found!"));
   }
 }
