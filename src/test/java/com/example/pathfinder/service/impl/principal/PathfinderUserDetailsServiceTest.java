2ebcda65c4849a766e847278da1bdc40a95847d9
wolfie <yavorhristozov@gmail.com>
2025-07-28 17:24:58 +0300

    Update PathfinderUserDetailsServiceTest.java

--- src/test/java/com/example/pathfinder/service/impl/principal/PathfinderUserDetailsServiceTest.java
+++ src/test/java/com/example/pathfinder/service/impl/principal/PathfinderUserDetailsServiceTest.java
@@ -15,7 +15,6 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-
 import java.time.LocalDateTime;
 import java.util.Collection;
 import java.util.Optional;
