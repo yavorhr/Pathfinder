19e497ef73875f6876eeb18d04e1d3354babba38
wolfie <yavorhristozov@gmail.com>
2025-07-28 17:25:26 +0300

    Update CategoryService.java

--- src/main/java/com/example/pathfinder/service/CategoryService.java
+++ src/main/java/com/example/pathfinder/service/CategoryService.java
@@ -3,10 +3,8 @@ package com.example.pathfinder.service;
 import com.example.pathfinder.model.entity.Category;
 import com.example.pathfinder.model.entity.enums.CategoryEnum;
 
-import java.util.Optional;
-
 public interface CategoryService {
 
-  Optional<Category> findByName(CategoryEnum categoryEnum);
+  Category findByName(CategoryEnum categoryEnum);
 
 }
