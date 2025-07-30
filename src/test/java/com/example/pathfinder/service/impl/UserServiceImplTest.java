89b456e9269956e088b02b8bbf25fadafabbfca8
wolfie <yavorhristozov@gmail.com>
2025-07-30 16:28:44 +0300

    Update UserServiceImplTest.java

--- src/test/java/com/example/pathfinder/service/impl/UserServiceImplTest.java
+++ src/test/java/com/example/pathfinder/service/impl/UserServiceImplTest.java
@@ -29,7 +29,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.security.crypto.password.PasswordEncoder;
-
 import java.time.LocalDateTime;
 import java.util.*;
 
