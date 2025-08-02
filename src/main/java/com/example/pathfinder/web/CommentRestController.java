b4d5f92aea726b8be44451a6650412b376c3413d
wolfie <yavorhristozov@gmail.com>
2025-08-02 14:52:42 +0300

    Update CommentRestController.java

--- src/main/java/com/example/pathfinder/web/CommentRestController.java
+++ src/main/java/com/example/pathfinder/web/CommentRestController.java
@@ -13,7 +13,6 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
-
 import java.net.URI;
 import java.util.List;
 
