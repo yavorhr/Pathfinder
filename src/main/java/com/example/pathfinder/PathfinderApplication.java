d8f9f53bf8ef3f43673ec5dad52d847ace3e87ad
wolfie <yavorhristozov@gmail.com>
2025-08-04 13:11:50 +0300

    Update PathfinderApplication.java

--- src/main/java/com/example/pathfinder/PathfinderApplication.java
+++ src/main/java/com/example/pathfinder/PathfinderApplication.java
@@ -3,7 +3,6 @@ package com.example.pathfinder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.annotation.Scheduled;
 
 @EnableScheduling
 @SpringBootApplication
