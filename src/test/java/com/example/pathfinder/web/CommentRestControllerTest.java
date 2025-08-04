90116c4023c9b097b45086602fdc5d13ac26dde2
wolfie <yavorhristozov@gmail.com>
2025-08-04 13:12:26 +0300

    Update CommentRestControllerTest.java

--- src/test/java/com/example/pathfinder/web/CommentRestControllerTest.java
+++ src/test/java/com/example/pathfinder/web/CommentRestControllerTest.java
@@ -29,8 +29,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.RequestPostProcessor;
-
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.Set;
