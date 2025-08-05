6747664e5bf40b10e63f671af0853fe837dc6735
wolfie <yavorhristozov@gmail.com>
2025-08-05 14:08:53 +0300

    Create HomeControllerTest.java
    testGetHomePage()

---
+++ src/test/java/com/example/pathfinder/web/HomeControllerTest.java
@@ -0,0 +1,26 @@
+package com.example.pathfinder.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class HomeControllerTest {
+
+  @Autowired
+  private MockMvc mockMvc;
+
+  @Test
+  void testGetHomePage_returnsHomePage() throws Exception {
+
+    mockMvc.perform(get("/")).
+            andExpect(status().isOk())
+            .andExpect(view().name("index"))
+            .andExpect(model().attributeExists("pictures"));
+  }
+}
