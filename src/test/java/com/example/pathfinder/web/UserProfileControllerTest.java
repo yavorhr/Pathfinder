ce7b900d6cef548c0d6d07f4d4c1709d26f156eb
wolfie <yavorhristozov@gmail.com>
2025-08-06 16:08:02 +0300

    Update UserProfileControllerTest.java
    updateProfile_returnsOkAndUpdatedViewModel()

--- src/test/java/com/example/pathfinder/web/UserProfileControllerTest.java
+++ src/test/java/com/example/pathfinder/web/UserProfileControllerTest.java
@@ -1,9 +1,11 @@
 package com.example.pathfinder.web;
 
+import com.example.pathfinder.model.binding.ProfileUpdateBindingModel;
 import com.example.pathfinder.model.entity.UserEntity;
 import com.example.pathfinder.model.entity.enums.GenderEnum;
 import com.example.pathfinder.model.entity.enums.LevelEnum;
 import com.example.pathfinder.repository.UserRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.AfterEach;
@@ -11,13 +13,16 @@ import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
@@ -28,9 +33,9 @@ public class UserProfileControllerTest {
   private MockMvc mockMvc;
   @Autowired
   private UserRepository userRepository;
-
+  @Autowired
+  ObjectMapper objectMapper;
   private UserEntity user;
-
 
   @BeforeEach
   void setup() {
@@ -56,6 +61,23 @@ public class UserProfileControllerTest {
                     Matchers.hasProperty("firstName", Matchers.equalTo(user.getFirstName()))));
   }
 
+  @Test
+  void updateProfile_returnsOkAndUpdatedViewModel() throws Exception {
+    ProfileUpdateBindingModel bindingModel = new ProfileUpdateBindingModel();
+    bindingModel.setFirstName("update_first_name");
+    bindingModel.setLastName("updated_last_name");
+    bindingModel.setId(user.getId());
+
+    String json = objectMapper.writeValueAsString(bindingModel);
+
+    mockMvc.perform(patch("/users/profile/edit")
+            .with(csrf())
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(json))
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.firstName").value("update_first_name"))
+            .andExpect(jsonPath("$.lastName").value("updated_last_name"));
+  }
 
   private UserEntity initUser(String username, String email, String password, String firstName, String lastName, LocalDate birthday, GenderEnum gender, LocalDateTime registrationDate, boolean isEnabled) {
     UserEntity testUser = new UserEntity();
@@ -72,7 +94,7 @@ public class UserProfileControllerTest {
             .setEnabled(isEnabled)
             .setLevel(LevelEnum.ADVANCED);
 
-    userRepository.save(testUser);
+    userRepository.saveAndFlush(testUser);
     return testUser;
   }
 }
