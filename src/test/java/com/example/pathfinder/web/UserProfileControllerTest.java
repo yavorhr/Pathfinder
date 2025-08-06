f84ddffe058e829a9c113fcdc2976ff0de428bc3
wolfie <yavorhristozov@gmail.com>
2025-08-06 16:11:26 +0300

    Update UserProfileControllerTest.java

--- src/test/java/com/example/pathfinder/web/UserProfileControllerTest.java
+++ src/test/java/com/example/pathfinder/web/UserProfileControllerTest.java
@@ -16,10 +16,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
-
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
