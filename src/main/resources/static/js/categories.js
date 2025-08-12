24c5d8cddacf36b507d72f06326f448782241ed8
wolfie <yavorhristozov@gmail.com>
2025-08-12 15:51:21 +0300

    Update categories.js

--- src/main/resources/static/js/categories.js
+++ src/main/resources/static/js/categories.js
@@ -1,21 +1,17 @@
 
-    // Wait for the DOM to fully load
     document.addEventListener("DOMContentLoaded", function() {
-    // Get elements
+
     const checkboxes = document.querySelectorAll('.category-checkbox');
     const addRouteButton = document.getElementById('addRouteButton');
 
-    // Function to check if at least one checkbox is selected
     function checkCheckboxes() {
     const isChecked = Array.from(checkboxes).some(checkbox => checkbox.checked);
 
     addRouteButton.disabled = !isChecked;
 }
-    // Add event listeners to all checkboxes
     checkboxes.forEach(checkbox => {
     checkbox.addEventListener('change', checkCheckboxes);
 });
 
-    // Initial check in case the form is pre-filled
     checkCheckboxes();
 });
