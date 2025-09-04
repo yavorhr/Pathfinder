c5261dd9f0b7c6b7e7b35bd154fcbabf53b023ba
wolfie <yavorhristozov@gmail.com>
2025-09-04 12:46:01 +0300

    Update categories.js
    fix css selector

--- src/main/resources/static/js/categories.js
+++ src/main/resources/static/js/categories.js
@@ -1,17 +1,17 @@
+document.addEventListener("DOMContentLoaded", function () {
 
-    document.addEventListener("DOMContentLoaded", function() {
-
-    const checkboxes = document.querySelectorAll('.category-checkbox');
+    const checkboxes = document.querySelectorAll('.categories input[type="checkbox"]');
     const addRouteButton = document.getElementById('addRouteButton');
 
     function checkCheckboxes() {
-    const isChecked = Array.from(checkboxes).some(checkbox => checkbox.checked);
+        const isChecked = Array.from(checkboxes).some(checkbox => checkbox.checked);
 
-    addRouteButton.disabled = !isChecked;
-}
+        addRouteButton.disabled = !isChecked;
+    }
+
     checkboxes.forEach(checkbox => {
-    checkbox.addEventListener('change', checkCheckboxes);
-});
+        checkbox.addEventListener('change', checkCheckboxes);
+    });
 
     checkCheckboxes();
 });
