786418e944b91f1b7b67fd8424bea43b9f5070af
wolfie <yavorhristozov@gmail.com>
2025-09-11 16:23:07 +0300

    Update RouteServiceImplTest.java

--- src/test/java/com/example/pathfinder/service/impl/RouteServiceImplTest.java
+++ src/test/java/com/example/pathfinder/service/impl/RouteServiceImplTest.java
@@ -27,6 +27,7 @@ import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 
+import java.io.IOException;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
@@ -259,7 +260,7 @@ public class RouteServiceImplTest {
   }
 
   @Test
-  void addNewRoute_returnsIdOfTheNewRoute() {
+  void addNewRoute_returnsIdOfTheNewRoute() throws IOException {
     //1. Arrange
     AddRouteServiceModel serviceModel = new AddRouteServiceModel();
     serviceModel.setAuthorId(admin.getId());
