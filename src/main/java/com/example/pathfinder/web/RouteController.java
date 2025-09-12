aa1ee67b19630e3c34b4ab57c52c238c975d3a8a
wolfie <yavorhristozov@gmail.com>
2025-09-12 16:03:14 +0300

    Update RouteController.java
    manual check if images are selected

--- src/main/java/com/example/pathfinder/web/RouteController.java
+++ src/main/java/com/example/pathfinder/web/RouteController.java
@@ -18,6 +18,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import java.io.IOException;
 import java.util.List;
@@ -109,7 +110,16 @@ public class RouteController {
                          BindingResult bindingResult,
                          RedirectAttributes redirectAttributes) throws IOException {
 
+
+    if (routeAddBindingModel.getPictures() == null ||
+            routeAddBindingModel.getPictures().isEmpty() ||
+            routeAddBindingModel.getPictures().stream().allMatch(MultipartFile::isEmpty)) {
+
+      bindingResult.rejectValue("pictures", "error.pictures", "At least one image is required");
+    }
+
     if (bindingResult.hasErrors()) {
+
       redirectAttributes
               .addFlashAttribute("routeAddBindingModel", routeAddBindingModel)
               .addFlashAttribute("org.springframework.validation.BindingResult.routeAddBindingModel", bindingResult);
